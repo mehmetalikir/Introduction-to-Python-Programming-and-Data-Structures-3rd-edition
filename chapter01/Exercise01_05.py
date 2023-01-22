9cfe15298b7fb07e871aebf7859e8d9630dd5314
hidirsezgin <hidirsezgin@gmail.com>
2023-01-22 18:07:43 +0300

    Compute expressions

---
+++ chapter01/Exercise01_05.py
@@ -0,0 +1,16 @@
+#(Compute expresions) Write a program that displays the results of
+# 14.8 - 2.4 x 3 + 7.6
+# / 5.2 + 3.3
+
+a = 14.8 - 2.4 * 3 + 7.6
+b = 5.2 + 3.3
+print( a / b )
+
+
+#print( (14.8 - 2.4 * 3 + 7.6) / (5.2 + 3.3) )
+
+
+
+
+
+
