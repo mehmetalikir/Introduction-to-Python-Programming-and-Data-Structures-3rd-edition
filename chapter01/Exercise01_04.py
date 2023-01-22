0ce89b8de4b9677c468fe7e243120ea994eb853f
mehmetalikir <mehmet@senax.com.au>
2023-01-23 01:48:41 +1100

    Print a table :smile:

---
+++ chapter01/Exercise01_04.py
@@ -0,0 +1,10 @@
+#(Print a table) Write a program that displays the followng table:
+
+print("a    a^2    a^3")
+print("1    1       1")
+print("2    4       8")
+print("3    9       27")
+print("4    16      64")
+
+
+
