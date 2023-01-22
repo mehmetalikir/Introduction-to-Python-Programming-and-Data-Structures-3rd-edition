26277f88a805b1d076997ad1cceb09b9ea07d33f
hidirsezgin <hidirsezgin@gmail.com>
2023-01-22 15:24:15 +0300

    Display there different messages

---
+++ chapter01/Exercise01_01.py
@@ -0,0 +1,4 @@
+#(Display there different messages) Write a program that
+# display Welcome to Python , Welcome to Computer Science,
+# and Programming is fun.
+print("Welcome to Python,\nWelcome to Computer Science,\nProgramming is fun.")
