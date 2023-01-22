11c130bff01838ca6641c94e3cd8efc469bdc032
mehmetalikir <mehmet@senax.com.au>
2023-01-23 00:12:57 +1100

    Display the same message five times 02 :loop:

--- chapter01/Exercise01_02.py
+++ chapter01/Exercise01_02.py
@@ -1,7 +1,4 @@
 #(Display the same message five times)
 # Write a program that displays Welcome to Python five times
-print("Welcome to Python")
-print("Welcome to Python")
-print("Welcome to Python")
-print("Welcome to Python")
-print("Welcome to Python")
+print("Welcome to Python\n" * 5)
+
