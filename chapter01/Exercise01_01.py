cba2da4b6cfabf68de4ca297d60985b24d31215c
hidirsezgin <hidirsezgin@gmail.com>
2023-01-22 15:43:38 +0300

    Display there different messages 02 :mag_right

--- chapter01/Exercise01_01.py
+++ chapter01/Exercise01_01.py
@@ -1,4 +1,7 @@
 #(Display there different messages) Write a program that
 # display Welcome to Python , Welcome to Computer Science,
 # and Programming is fun.
-print("Welcome to Python,\nWelcome to Computer Science,\nProgramming is fun.")
+a = "Welcome to Python"
+b = "Welcome to Computer Science"
+c = "Programming is fun."
+print("{}\n{}\n{}".format(a,b,c))
