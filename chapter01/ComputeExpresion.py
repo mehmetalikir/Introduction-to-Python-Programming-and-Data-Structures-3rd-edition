e60964057a521c8f13bca8cf2ae53cfc2767bad4
hidirsezgin <hidirsezgin@gmail.com>
2023-01-29 11:20:36 +0300

    Compute expresions in another way

--- chapter01/ComputeExpresion.py
+++ chapter01/ComputeExpresion.py
@@ -1,7 +1,7 @@
 #(Compute expresions) Write a program that displays the results of
 # 14.8 - 2.4 x 3 + 7.6 / 5.2 + 3.3
 
-#Compute expresions
+#Compute expresions by book
 a = 14.8 - 2.4 * 3 + 7.6
 b = 5.2 + 3.3
 print("Compute expresions:" , a / b )
@@ -9,6 +9,10 @@ print("Compute expresions:" , a / b )
 #Or
 print( "Compute expresions:", (14.8 - (2.4 * 3 )+ 7.6) / (5.2 + 3.3) )
 
+#Or
+a = 14.8 - 2.4 * 3 + 7.6
+b = 5.2 + 3.3
+print("Compute expresions:","{:,.3f}".format(a / b))
 
 
 
--- chapter01/PrintATable.py
+++ chapter01/PrintATable.py
@@ -8,5 +8,11 @@ print("2    4       8")
 print("3    9       27")
 print("4    16      64")
 
+print("------------------------------")
+print("Table with loop, Exercise01_04")
+print("a \t\t a^2\t a")
+for i in range(1, 5):
+    print("{}\t\t {}\t\t {}".format(i, i ** 2, i ** 3))
+
 
 
