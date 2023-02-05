e17f84f0856e551e6d6b72b97892767249f2efba
hidirsezgin <hidirsezgin@gmail.com>
2023-02-05 14:59:22 +0300

    Turtle: draw four square

--- chapter01/TurtleDrawFourSquares.py
+++ chapter01/TurtleDrawFourSquares.py
@@ -1,3 +1,27 @@
 #Exercise01_12
-''' (Turtle: draw four squares) Write a program that draws four squares in the center 
+''' (Turtle: draw four squares) Write a program that draws four squares in the center
 of the screen, as shown in Figure 1.12a'''
+
+import turtle
+
+# slowest speed
+turtle.speed(1)
+
+# turtle draw perimeter of square
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+
+# turtle draws inner edges of square
+turtle.right(180)
+turtle.forward(75)
+
+turtle.left(90)
+turtle.forward(75)
+
+turtle.right(90)
+turtle.forward(75)
