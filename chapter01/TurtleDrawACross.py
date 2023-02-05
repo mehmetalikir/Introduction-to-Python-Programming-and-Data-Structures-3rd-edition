30ef349011372aaed4ca1319f6ee7ca08c994fa4
mehmetalikir <mehmet@senax.com.au>
2023-02-05 23:17:36 +1100

    Turtle: draw a cross :negative_squared_cross_mark:

---
+++ chapter01/TurtleDrawACross.py
@@ -0,0 +1,22 @@
+#Exercise01_13
+''' (Turtle: draw a cross) Write a program that draws four squares in the center
+of the screen, as shown in Figure 1.12b'''
+
+import turtle
+
+# slowest speed
+turtle.speed(1)
+
+# turtle draws a cross
+turtle.forward(150)
+turtle.right(180)
+turtle.forward(75)
+turtle.right(90)
+turtle.forward(75)
+turtle.left(180)
+turtle.forward(150)
+
+
+
+
+
