64e33200a834191d55a586bc6ab2f73908e0e0bc
mehmetalikir <mehmet@senax.com.au>
2023-02-05 23:53:14 +1100

    Turtle: draw two triangle :small_red_triangle: :small_red_triangle_down:

---
+++ chapter01/TurtleDrawTwoTriangles.py
@@ -0,0 +1,27 @@
+#Exercise01_13
+''' (Turtle: draw two triangle) Write a program that draws four squares in the center
+of the screen, as shown in Figure 1.12b'''
+
+import turtle
+
+# slowest speed
+turtle.speed(1)
+
+# turtle draws two triangle
+turtle.left(-75)
+turtle.forward(75)
+turtle.right(-75)
+turtle.forward(-68)
+turtle.left(57)
+turtle.forward(150)
+
+turtle.left(125)
+turtle.forward(70)
+turtle.left(125)
+turtle.forward(70)
+
+
+
+
+
+
