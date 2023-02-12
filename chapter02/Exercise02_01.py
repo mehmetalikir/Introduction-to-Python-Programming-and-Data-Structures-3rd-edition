468a96a914dcb0f49651b8e029e3f6b4487d0dca
mehmetalikir <mehmet@senax.com.au>
2023-02-12 22:04:00 +1100

    Start Chater 2 :beginner:

---
+++ chapter02/Exercise02_01.py
@@ -0,0 +1,16 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+#Exercise02_01
+''' (Convert km/h to m/s) Write a program that reads a value in kilometers pe hour
+from the console and displays it in meter per second. The general formula for
+the conversion is as follows:'''
+
+#Enter sped in km/h
+km_h = float(("Enter sped in km/h : "))
+
+
+
+
+
