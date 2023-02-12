ac0f419e439a968cac53fbdac9601b0a8094563f
hidirsezgin <hidirsezgin@gmail.com>
2023-02-12 23:08:56 +0300

    Average acceleration # Conflicts: #	chapter02/Exercise02_01_temp.py

---
+++ chapter02/Exercise02_16.py
@@ -0,0 +1,24 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+#Exercise02_16
+#(Phisics: acceleration)
+"""
+Average acceleration is defined as the change of velocity
+divided by the time taken to make the change, as shown in
+the following formula:
+a = (v1 - v0) / t
+"""
+#Enter the starting velocity in meters/second
+v0 = float(input("Enter the starting velocity in meters/second:\t"))
+
+#Enter the ending velocity in meters/second
+v1 = float(input("Enter the ending velocity in meters/second:\t"))
+
+#Enter the time span in seconds
+t = float(input("Enter the time span in seconds:\t"))
+
+a = (v1 - v0) / t
+
+print("The average acceleration is:",a)
