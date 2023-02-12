d5f50a0811ef4f74b6c63c0bc96be908cf01f5ee
mehmetalikir <mehmet@senax.com.au>
2023-02-12 22:18:51 +1100

    Convert km/h to m/s :beginner::beginner:

--- chapter02/Exercise02_01.py
+++ chapter02/Exercise02_01.py
@@ -7,10 +7,12 @@
 from the console and displays it in meter per second. The general formula for
 the conversion is as follows:'''
 
+# 1 km / 1 h = 1000 m / 3600 s
+
 #Enter sped in km/h
-km_h = float(("Enter sped in km/h : "))
-
-
+km_h = int(input("Enter sped in km/h : "))
+result = km_h * (1000 / 3600)
+print(km_h, ' is equal to ', result )
 
 
 
