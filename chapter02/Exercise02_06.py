d9931776afd622624dc77d4699d3c5e67c6c21a3
hidirsezgin <hidirsezgin@gmail.com>
2023-02-12 15:50:43 +0300

    Financial application: monetary units # Conflicts: #	chapter02/Exercise02_01_temp.py

---
+++ chapter02/Exercise02_06.py
@@ -0,0 +1,13 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+"""
+Exercice02_06
+(Financial application: monetary units)
+To fix the possible loss of accuracy when coverting a float value
+to an int value. For example, the input 1156 represent 11 dollars and 56 cents.
+"""
+cent = int(input("In cents, enter the amount of money:\t"))
+doll = cent // 100
+cnt = cent % 100
+print("{} cents is: {} dollars and {} cents".format(cent, doll, cnt))
