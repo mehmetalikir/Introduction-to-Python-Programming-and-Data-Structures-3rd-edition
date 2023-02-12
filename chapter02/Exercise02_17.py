af28a18f3ad9ab081c083947cd5febd4b7f884cf
hidirsezgin <hidirsezgin@gmail.com>
2023-02-12 23:48:13 +0300

    (Health application: compute BMI) # Conflicts: #	chapter02/Exercise02_01_temp.py

---
+++ chapter02/Exercise02_17.py
@@ -0,0 +1,24 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+#Exercise02_17
+#(Health application: compute BMI)
+"""
+It can be calculated by taking your weight in klograms
+and dividing it by the square of your height in meters.
+one pound is 0.45359237 kg
+one inch is 0.0254 m.
+"""
+#Enter weight in pounds
+w = float(input("Enter weight in pounds (for example: 95.5):\t")) \
+    * 0.45359237
+
+#Enter height in inches
+h = float(input("Enter height in inches (for example: 50):\t")) \
+    * 0.0254
+
+#Calculation the BMI:
+bmi = w / (h ** 2)
+
+print("The BMI is:",bmi)
