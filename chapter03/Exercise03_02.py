0e5330204cdb14057d6b824d0473ec92a1a249c5
mehmetalikir <mehmet@senax.com.au>
2023-03-09 14:40:54 +1100

    Generate random numbers

--- chapter03/Exercise03_02.py
+++ chapter03/Exercise03_02.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Game: square of a number) The program in Listing 3.1, generates two integers
+and prompts the user to enter the sum of these two integers.
+Revise the program to generate one single-digit integer and prompt the user to
+enter the square of the integers.'''
+
+import random
+
+# Generate random numbers
+number1 = random.randint(0,9)
+number2 = random.randint(0,9)
+
+# Prompt the user to enter an answer
+answer = eval(input("What is " + str(number1) + " + " + str(number2) + "? "))
+
+# Display result
+print(number1, "+", number2, "=", answer, "is", number1 + number2 == answer)
