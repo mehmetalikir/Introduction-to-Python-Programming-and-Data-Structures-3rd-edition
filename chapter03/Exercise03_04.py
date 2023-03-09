74a210afaaa189b8a0c9dcb1eee50c63649b6599
mehmetalikir <mehmet@senax.com.au>
2023-03-09 15:31:47 +1100

    Game: multiply two  :1234:

--- chapter03/Exercise03_04.py
+++ chapter03/Exercise03_04.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Game: multiply two numbers) The program in Listing 3.1 generates two integers
+and prompts the user to enter the sum of these two integers. Revise the program
+to generate two two-digit nonzero integers and prompt the user to enter the
+product of these two integers'''
+
+import random
+
+# Generate random numbers
+number1 = random.randint(10,100)
+number2 = random.randint(10,100)
+
+# Prompt the user to enter an answer
+answer = eval(input("What is " + str(number1) + " * " + str(number2) + "? "))
+
+# Display result
+print(number1, "*", number2, "=", answer, "is", number1 * number2 == answer)
