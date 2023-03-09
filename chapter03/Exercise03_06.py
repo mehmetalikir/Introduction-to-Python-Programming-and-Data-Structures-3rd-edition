f2f2966a3214fd4fa9110eb5f4a44163fc263ae0
mehmetalikir <mehmet@senax.com.au>
2023-03-09 16:52:22 +1100

    Health application: BMI :hospital

--- chapter03/Exercise03_06.py
+++ chapter03/Exercise03_06.py
@@ -6,21 +6,28 @@
 let users enter their weight in pound and their in feet and inches. For example,
 if a person is 5 feet and 10 inches, you will enter 5 for feet and 10 for inches.'''
 
-# Prompt the user to enter weight in pounds
-weight = float(input("Please enter weight in pounds: "))
-
-# Prompt the user to enter height in inches
-height = float(input("Please enter height in inches"))
+import math
 
 KILOGRAMS_PER_POUND = 0.45359237 # Constant
 METERS_PER_INCH = 0.0254 # Constant
+FEET_PER_INCH = 0.0833333 # Constant
+
+# Prompt the user to enter weight in pounds
+weight = float(input("Please enter weight in pounds: "))
+
+# Prompt the user to enter feet
+feet = float(input("Please enter feet: "))
+
+# Prompt the user to enter feet
+inches = float(input("Please enter inches: "))
+inches += feet / FEET_PER_INCH
 
 # Compute BMI
 weightInKilograms = weight * KILOGRAMS_PER_POUND
-heightInMeters = height * METERS_PER_INCH
-bmi = weightInKilograms / (heightInMeters * heightInMeters)
+heightInMeters = inches * METERS_PER_INCH
+bmi = weightInKilograms / math.pow(heightInMeters, 2)
 
-#Display result
+# Display result
 print("BMI is", bmi)
 if bmi < 18.5:
     print("Underweight")
