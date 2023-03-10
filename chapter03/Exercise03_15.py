72abc8c5a6a587a2609d88adc5a8fa0613bc4600
mehmetalikir <mehmet@senax.com.au>
2023-03-10 21:48:23 +1100

    Game: black or red :black_circle: :left_right_arrow: :red_circle:

--- chapter03/Exercise03_15.py
+++ chapter03/Exercise03_15.py
@@ -3,3 +3,18 @@
 # GitHub: /hidirsezgin, /mehmetalikir
 
 '''() Write a program that'''
+
+import random
+
+red = 0
+black = 0
+for i in range(1_000_000_000):
+    number = random.randint(0,1)
+    if number == 0:
+        red += 1
+    else:
+        black += 1
+
+print("Red:", red, "Black:", black)
+
+
