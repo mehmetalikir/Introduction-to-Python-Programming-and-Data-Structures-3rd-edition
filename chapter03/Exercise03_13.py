1230be4c3af50689ad2fcedbe41cecd8c0f61352
mehmetalikir <mehmet@senax.com.au>
2023-03-10 20:49:14 +1100

    Fix formula for single filers

--- chapter03/Exercise03_13.py
+++ chapter03/Exercise03_13.py
@@ -9,6 +9,8 @@ the source code for the other filing statuses.'''
 import sys
 
 # Prompt the user to enter filing status
+print("0-single filer, 1-married jointly,\n"
+      "2-married separately, 3-head of household")
 status = int(input("Please enter the filing status: "))
 
 # Prompt the user to enter taxable income
@@ -25,21 +27,21 @@ if status == 0:  # Compute tax for single filers
     elif income <= 82250:
         tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25
     elif income <= 171550:
-        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25 + (income - 82250) * 0.28
+        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (income - 82250) * 0.28
     elif income <= 372950:
-        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25 + (income - 82250) * 0.28 + (income - 171550) * 0.33
+        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (
+                income - 171550) * 0.33
     else:
-        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25 + (income - 82250) * 0.28 + (
-                    income - 171550) * 0.33 + (income - 372950) * 0.35
-elif status == 1: # Compute tax for married file jointly
-    print("TODO")
-elif status == 2: # Compute tax for married separately
-    print("TODO")
-elif status == 3: # Compute tax for head of household
-    print("TODO")
+        tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (
+                372950 - 171550) * 0.33 + (income - 372950) * 0.35
+
+elif status == 1:  # Compute tax for married file jointly
+
+elif status == 2:  # Compute tax for married separately
+
+elif status == 3:  # Compute tax for head of household
+
 else:
     print("Please enter valid status")
     sys.exit()
 print("Tax is", tax)
-
-
