1d53fdd938cbe10530ea5433fd5a3ec5e2c2512c
mehmetalikir <mehmet@senax.com.au>
2023-03-15 22:50:26 +1100

    Palindrome number

--- chapter03/Exercise03_26.py
+++ chapter03/Exercise03_26.py
@@ -2,4 +2,24 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Palindrome number) Write a program that prompts the user to enter a three-digit
+integer and determines whether it is a palindrome number. A number is palindrome
+if it reads the same from right to left and from left to right.'''
+
+# Prompt the user to enter a three-digit integer
+number = int(input("Please enter a three-digit integer: "))
+str = number
+
+# Determine whether it is a palindrome number
+digit1 = number % 10
+number //=  10
+digit2 = number % 10
+number //=  10
+digit3 = number
+isPalindrome = digit1 == digit3
+
+# Display result
+if isPalindrome:
+    print(str,"is a palindrome")
+else:
+    print(str,"is not a palindrome")
