063365ecf6eb03caaf8a242e7af69a05e16bde2f
mehmetalikir <mehmet@senax.com.au>
2023-03-16 01:15:18 +1100

    Random point

--- chapter03/Exercise03_34.py
+++ chapter03/Exercise03_34.py
@@ -2,4 +2,15 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+
+'''(Random point) Write a program that displays a random coordinate in a square.
+The square is centered at (0, 0) and the length of each side is 100'''
+
+import random
+
+# Generate random points
+x = random.randint(-50,50)
+y = random.randint(-50,50)
+
+# Display coordinate
+print("Random coordinate: (", x, ",", y, ")")
