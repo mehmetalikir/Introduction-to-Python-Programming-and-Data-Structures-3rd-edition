904a0ddba3bcc244292eb23e8ef41de7481bf311
mehmetalikir <mehmet@senax.com.au>
2023-03-16 17:07:32 +1100

    Geometry: area of a hexagon

--- chapter04/Exercise04_04.py
+++ chapter04/Exercise04_04.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Geometry: area of a hexagon) The area of a hexagon can be computed using the
+following formula (s is the length of a side):
+area = 6 x s^2 / 4 x tan(pi/6)
+Write a program that prompts the user to enter the side of a hexagon and displays
+its area.'''
+
+import math
+
+# Prompt the user to enter the side of a hexagon
+s = float(input("Please enter the side: "))
+
+# Compute the area of a hexagon
+area = (6 * math.pow(s, 2)) / (4 * math.tan(math.pi / 6))
+
+# Display result
+print(f"The are of the hexagon is {area}")
