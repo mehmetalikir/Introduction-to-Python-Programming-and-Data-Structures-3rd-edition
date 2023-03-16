e442ab0572347ce60bbbfabf8ab8c59ad068feba
mehmetalikir <mehmet@senax.com.au>
2023-03-16 23:21:14 +1100

    Find the binary code of a character :one: :one::one: :zero: :one: :one: :one:

--- chapter04/Exercise04_19.py
+++ chapter04/Exercise04_19.py
@@ -2,4 +2,13 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Find the binary code of a character) Write a program that receives a character and
+displays its binary code.'''
+
+
+# Prompt the user to enter a character
+c = (input("Please enter a character: "))
+
+# Display result
+print(f"The binary code of {c} is",'{:0b}'.format(ord(c)))
+
