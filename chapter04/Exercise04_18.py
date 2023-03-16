3083606cdae4998eee3fb5680307c27d9db6a9a9
mehmetalikir <mehmet@senax.com.au>
2023-03-16 22:58:23 +1100

    Find the character of an ASCII code

--- chapter04/Exercise04_18.py
+++ chapter04/Exercise04_18.py
@@ -2,4 +2,12 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Find the character of an ASCII code) Write a program that receives an ASCII code
+(an integer between 0 and 127) and displays its character.For example, if the user
+enters 97, the program displays the character a'''
+
+# Prompt the user to enter an integer between 0 and 127
+num = int(input("Please enter an ASCII code: "))
+
+# Display result
+print(f"The character for ASCII code {num} is", chr(num))
