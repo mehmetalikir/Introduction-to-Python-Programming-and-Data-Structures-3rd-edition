6e34c421f62de9eceeb9f126f19b1985ae44773c
mehmetalikir <mehmet@senax.com.au>
2023-03-17 00:41:48 +1100

    Random character generator

--- chapter04/Exercise04_20.py
+++ chapter04/Exercise04_20.py
@@ -2,4 +2,14 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Random character) Write a program that displays a random lowercase or
+uppercase letter'''
+
+import random # Import random module to use the random functions
+
+# Randomize the ASCII value of the lowercase and uppercase
+lowercase = random.randint(97,122)
+uppercase = random.randint(65,90)
+
+# Display result
+print(f"Random letters are {chr(lowercase)} and {chr(uppercase)}")
