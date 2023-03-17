476d95472451cb690e41e030ef5b3af6fdaaa9e8
mehmetalikir <mehmet@senax.com.au>
2023-03-17 23:13:40 +1100

    Phone key pads

--- chapter04/Exercise04_27.py
+++ chapter04/Exercise04_27.py
@@ -2,4 +2,34 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Phone key pads) The international standard letter/number mapping found on the
+telephone is shown below:
+
+Write a program that prompts the user to enter a letter and displays its corresponding
+number.'''
+
+# Prompt the use for input
+c = str(input("Please enter an uppercase letter: ")).upper()
+
+# Check its corresponding number
+if 'Z'>= c >= 'W':
+    number = 9
+elif 'V'>= c >= 'T':
+    number = 8
+elif 'S'>= c >= 'P':
+    number = 7
+elif 'O'>= c >= 'M':
+    number = 6
+elif 'L'>= c >= 'J':
+    number = 5
+elif 'I'>= c >= 'G':
+    number = 4
+elif 'F'>= c >= 'D':
+    number = 3
+elif 'C'>= c >= 'A':
+    number = 2
+else:
+    print("1 0 * #")
+
+# Display result
+print("The corresponding number is ", number)
