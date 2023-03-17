53d3a67028dac19d62a44e657ba4a2b98bcd5148
mehmetalikir <mehmet@senax.com.au>
2023-03-17 14:31:25 +1100

    pH calculator

--- chapter04/Exercise04_23.py
+++ chapter04/Exercise04_23.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(pH calculator) Write a program that prompts the user to enter a number in range
+of 1 - 14 and displays a message as acidic (less than 7), neutral(7) or alkaline
+(more than 7), otherwise invalid if the input iso ut of range'''
+
+# Prompt the user to enter a number in range of 1 - 14
+num = float(input("Please enter a pH value between 1 to 14: "))
+
+# Display result
+if num < 7 and num >= 1:
+    print(f"{num} is acidic")
+elif num == 7:
+    print(f"{num} is neutral")
+elif num > 7 and num <= 14:
+    print(f"{num} is alkaline")
+else:
+    print(f"{num} is invalid")
