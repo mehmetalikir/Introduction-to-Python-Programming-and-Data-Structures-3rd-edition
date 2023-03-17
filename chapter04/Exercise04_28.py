6ca554eea64ee6fc2be55f8c3ec5a20b6d57644a
mehmetalikir <mehmet@senax.com.au>
2023-03-17 23:39:48 +1100

    Process a string

--- chapter04/Exercise04_28.py
+++ chapter04/Exercise04_28.py
@@ -2,4 +2,13 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Process a string) Write a program that prompts the user to enter a string and displays
+its length and its last character, and total number of spaces.'''
+
+# Prompt the user for input
+s = str(input("Please enter a string: ")) # Welcome to Python Programming
+
+# Display result
+print("THe length of string is", len(s))
+print("The last character of string is", s[-1])
+print("Total number of spaces is",s.count(" "))
