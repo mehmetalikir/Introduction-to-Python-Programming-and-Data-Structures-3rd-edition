2873fa0b9c3e873427656fdcf2bcfa7ffb8c297f
mehmetalikir <mehmet@senax.com.au>
2023-03-17 16:14:42 +1100

    Student major and status

--- chapter04/Exercise04_26.py
+++ chapter04/Exercise04_26.py
@@ -2,4 +2,34 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Student major and status) Write a program that prompts the user to enter two
+characters and displays the major and status represented in the characters. The first
+character indicates the major and the second is number character 1, 2, 3, 4, which
+indicates whether a student is a freshman, sophomore, junior, or senior. Suppose
+the following characters are used to denote the majors:
+M: Mathematics
+C: Computer Science
+I: Information Technology'''
+
+# Prompt the user to enter two characters
+c = str(input("Please enter two characters: ")).upper()
+
+major = ''
+status = ''
+
+if c.startswith('M'):
+    major = "Mathematics"
+if c.startswith('C'):
+    major = "Computer Science"
+if c.startswith('I'):
+    major = "Information Technology"
+if c.endswith('1'):
+    status = "Freshman"
+if c.endswith('2'):
+    status = "Sophomore"
+if c.endswith('3'):
+    status = "Junior"
+if c.endswith('4'):
+    status = "Senior"
+
+print("",major,"\n",status)
