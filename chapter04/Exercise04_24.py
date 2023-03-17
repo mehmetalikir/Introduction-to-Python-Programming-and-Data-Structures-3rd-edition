e3817b9f39844d7601cd88418df5dc54526be1b0
mehmetalikir <mehmet@senax.com.au>
2023-03-17 15:02:03 +1100

    Vowel or consonant?

--- chapter04/Exercise04_24.py
+++ chapter04/Exercise04_24.py
@@ -2,4 +2,21 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Vowel or consonant?) Assume letters A/a, E/e, I/i, O/o, and U/u as the vowels.
+Write a program that prompts the user to enter a letter and check whether the
+letter is a vowel or consonant.'''
+
+# Prompt the user to enter a letter
+c = (str(input("Please enter a letter (A-Z or a-z): ")))
+
+# Convert the letter to uppercase
+c = c.upper()
+
+# Display result
+if 'A' <= c <= 'Z':
+    if c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U':
+        print(f"{c} is a vowel")
+    else:
+        print(f"{c} is a consonant")
+else:
+    print("Please enter valid letter")
