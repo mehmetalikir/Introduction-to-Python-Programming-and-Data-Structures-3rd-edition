bb6e2e7e4f42a2a5c19fa0f94c697669c7ba44a3
mehmetalikir <mehmet@senax.com.au>
2023-03-18 22:29:54 +1100

    Random uppercase strings

--- chapter04/Exercise04_33.py
+++ chapter04/Exercise04_33.py
@@ -2,4 +2,22 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Random uppercase strings) Write a program that generates a random string of
+four characters from the 26 uppercase letters'''
+
+import random
+
+# Randomize ASCII codes for uppercase
+ch0 = chr(random.randint(65,90))
+ch1 = chr(random.randint(65,90))
+ch2 = chr(random.randint(65,90))
+ch3 = chr(random.randint(65,90))
+
+# Generate a random string of four characters from the 26 uppercase letters
+s = str(ch0 + ch1 + ch2 + ch3)
+
+# Display result
+print("Generated random string:", s)
+
+
+
