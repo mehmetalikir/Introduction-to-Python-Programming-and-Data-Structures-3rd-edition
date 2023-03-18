7aeb6ab1744218033de5d40edd2c487c0ab8c02c
mehmetalikir <mehmet@senax.com.au>
2023-03-18 23:13:42 +1100

    Order three cities feat: Simultaneous assignment :four_leaf_clover:

--- chapter03/Exercise03_08.py
+++ chapter03/Exercise03_08.py
@@ -12,39 +12,30 @@ b = float(input("Please enter number2: "))
 c = float(input("Please enter number3: "))
 
 # Display result after sorting
-temp = 0
-if b > a or c > a:
-    temp = a
-    a = b
-    b = temp
-    if c > a:
-        temp = a
-        a = c
-        c = temp
+if b > a:
+    b, a = a, b # Simultaneous assignment
 if c > b:
-    temp = b
-    b = c
-    c = temp
+    c, b = b, c
+if b > a:
+    b, a = a, b
 
 print("The sorted numbers in decreasing order are", a, b, c)
-
-
 # for increasing order a < b < c , a < c < b , b < a < c, b < c < a, c < a < b, c < b < a
 
 
-num1, num2, num3, num4 = 2, 9, 7, 14
+a, b, c, d = 2, 9, 7, 14
 # Sort 4 integer numbers in ascending order
-if num2 < num1:
-    num2, num1 = num1, num2 # Simultaneous assignment
-if num3 < num2:
-    num3, num2 = num2, num3
-if num2 < num1:
-    num2, num1 = num1, num2
-if num4 < num3:
-    num4, num3 = num3, num4
-if num3 < num2:
-    num3, num2 = num2, num3
-if num2 < num1:
-    num2, num1 = num1, num2
-print(num1,num2,num3,num4)
+if b < a:
+    b, a = a, b # Simultaneous assignment
+if c < b:
+    c, b = b, c
+if b < a:
+    b, a = a, b
+if d < c:
+    d, c = c, d
+if c < b:
+    c, b = b, c
+if b < a:
+    b, a = a, b
+print(a, b, c, d)
 
--- chapter04/Exercise04_34.py
+++ chapter04/Exercise04_34.py
@@ -2,4 +2,21 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Order three cities) Write a program that prompts the user to enter there cities
+and displays them in ascending order.'''
+
+# Prompts the user to enter there cities
+str1 = str(input("Please enter first city: "))
+str2 = str(input("Please enter second city: "))
+str3 = str(input("Please enter third city: "))
+
+# Swap strings in ascending order
+if str2 < str1:
+    str2, str1 = str1, str2 # Simultaneous assignment
+if str3 < str2:
+    str3, str2 = str2, str3
+if str2 < str1:
+    str2, str1 = str1, str2
+
+# Display result
+print(str1, str2, str3)
