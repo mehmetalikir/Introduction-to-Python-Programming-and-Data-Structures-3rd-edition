212e81b81e11bef2fe79569d44c25f9ba72478a4
mehmetalikir <mehmet@senax.com.au>
2023-03-18 12:46:49 +1100

    Business: check ISBN-10

--- chapter04/Exercise04_29.py
+++ chapter04/Exercise04_29.py
@@ -2,4 +2,45 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Business: check ISBN-10) An ISBN-10 (International Standard Book Number)
+consists of 10 digits: d1d2d3d4d5d6d7d8d9d10. The last digit, d10, is a checksum,
+which is calculated from the other nine digits using the following formula:
+(d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 +
+d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9) % 11
+If the checksum is 10, the last digit is denoted as X according to the ISBN-10
+convention. Write a program that prompts the user to enter the first 9 digits and
+displays the 10-digit ISBN (including leading zeros).'''
+
+# Prompt the user to enter the first 9 digits
+isbn = str(input("Please enter the first 9 digit of an ISBN number as a string: "))
+
+isbn = int(isbn)
+
+# Compute the checksum
+d1 = isbn // 100000000
+digit = isbn % 100000000
+d2 = digit // 10000000
+digit %= 10000000
+d3 = digit // 1000000
+digit %= 1000000
+d4 = digit // 100000
+digit %= 100000
+d5 = digit // 10000
+digit %= 10000
+d6 = digit // 1000
+digit %= 1000
+d7 = digit // 100
+digit %= 100
+d8 = digit // 10
+digit %= 10
+d9 = digit
+
+s = ""
+d10 = (d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 + d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9) % 11
+if d10 == 10:
+    s = "X"
+else:
+    s = d10
+
+# Display result
+print("The ISBN-10 number is ", d1,d2,d3,d4,d5,d6,d7,d8,d9,s)
