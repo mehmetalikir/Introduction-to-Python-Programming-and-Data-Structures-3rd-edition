adc2e7d01ca03e3669cf75e1baae6dec3f953062
mehmetalikir <mehmet@senax.com.au>
2023-03-21 01:36:12 +1100

    Conversion from gallons to litters

--- chapter05/Exercise05_05.py
+++ chapter05/Exercise05_05.py
@@ -2,4 +2,17 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Conversion from gallons to litters) Write a program that displays the following
+two tables side by side (note that 1 gallon is 3.785 liters):'''
+
+gallon = 2 # Initialize variable
+litter = 10 # Initialize variable
+
+# Display header for table
+print("Gallons      Liters  |  Gallons      Liters")
+
+# Display table
+while gallon <= 100:
+    print(gallon,"          ", format((gallon * 3.785), ".1f" ), "   | ", litter,"          ", format((litter / 3.785), ".2f" ))
+    gallon += 2
+    litter += 3
