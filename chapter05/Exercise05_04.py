7c4c235d6dceb13e68ae555f9c4176a26ca1703e
mehmetalikir <mehmet@senax.com.au>
2023-03-21 01:27:32 +1100

    Conversion from inches to centimeters

--- chapter05/Exercise05_04.py
+++ chapter05/Exercise05_04.py
@@ -2,4 +2,17 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Conversion from inches to centimeters) Write a program that displays the following
+table (note that 1 inch is 2.54 centimeters):'''
+
+inch = 1 # Initialize variable
+
+# Display header for table
+print("Inches      Centimeters")
+
+# Display table
+while inch <= 50:
+    print(inch, "          ", format((inch * 2.54), ".2f"))
+    inch += 1
+
+
