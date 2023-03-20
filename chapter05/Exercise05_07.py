01ec2bbced8b41e1212500cfa83878c85257b620
mehmetalikir <mehmet@senax.com.au>
2023-03-21 02:10:04 +1100

    Use trigonometric functions

--- chapter05/Exercise05_07.py
+++ chapter05/Exercise05_07.py
@@ -2,4 +2,18 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Use trigonometric functions) Print the following table to display the cos value
+and tan value of degrees from 0 to 360 with increments of 20 degrees. Round
+the value to keep four digits after the decimal point'''
+
+import math
+
+degree = 0 # Initialize variable
+
+# Display header for table
+print("Degree     Cos      Tan")
+
+# Display table
+while degree <= 360:
+    print(degree, "     ", round(math.cos(math.radians(degree)), 4), "      ", round(math.tan(math.radians(degree)), 4))
+    degree += 20
