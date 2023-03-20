be52253137efe986c1c8ad28a6a06f0ad8681daa
mehmetalikir <mehmet@senax.com.au>
2023-03-21 02:21:41 +1100

    Use the math.pow function

--- chapter05/Exercise05_08.py
+++ chapter05/Exercise05_08.py
@@ -2,4 +2,16 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Use the math.pow function) Write a program that prints the following table
+using the pow function in the math module'''
+import math
+
+number = 0 # Initialize variable
+
+# Display header for table
+print("Inches      Centimeters")
+
+# Display table
+while number <= 48:
+    print(number, "         ", round(math.pow(number, 1 / 3), 4))
+    number += 4
