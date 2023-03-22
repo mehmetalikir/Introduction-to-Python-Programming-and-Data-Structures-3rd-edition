41815db39bcdfcc121658193d55e5239c92b1b0f
mehmetalikir <mehmet@senax.com.au>
2023-03-23 01:30:50 +1100

    Find the largest integer n such that n^3 > 12000

--- chapter05/Exercise05_15.py
+++ chapter05/Exercise05_15.py
@@ -2,4 +2,14 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Find the largest integer n such that n^3 > 12000) Use a while loop to find the largest
+integer n such that n^3 is less than 12000'''
+
+# Assign variable
+n = 0
+
+# Find the largest integer n such that n^3 > 12000 and display result
+while n * n * n < 12000:
+    n += 1
+print("n:", n)
+
