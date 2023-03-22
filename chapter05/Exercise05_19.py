1a9735c359e94d9f1b23839bfd2beef9afc0cc42
mehmetalikir <mehmet@senax.com.au>
2023-03-23 03:03:09 +1100

    Display pyramid

--- chapter05/Exercise05_19.py
+++ chapter05/Exercise05_19.py
@@ -3,4 +3,26 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Display pyramid) Write a program that prompts the user to enter an integer from
+1 to 15 and displays a pyramid, as shown in the following sample run:'''
+
+# Constant
+NUM = 7
+
+for i in range(0,NUM):
+    j = NUM - 1 - i
+    while j > 0:
+        print(end = " ")
+        j -= 1
+
+    k = i + 1
+    while k > 0:
+        print(k, end = " ")
+        k -= 1
+
+    l = 2
+    while l < i + 2:
+        print(l, end = " ")
+        l += 1
+    print(end= " ")
+
