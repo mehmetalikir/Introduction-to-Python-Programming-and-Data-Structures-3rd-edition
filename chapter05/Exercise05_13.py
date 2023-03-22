7d1948bd38ecb58df44e4ef8ca9cd6126482b1da
mehmetalikir <mehmet@senax.com.au>
2023-03-23 00:18:25 +1100

    Find numbers divisible by 11 or 17, but not both

--- chapter05/Exercise05_13.py
+++ chapter05/Exercise05_13.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Find numbers divisible by 11 or 17, but not both) Write a program that displays, five numbers
+pers line, all the numbers from 1000 to 1100 that are divisible by 11 or 17, but not both.
+The numbers are separated by exactly one tab.'''
+
+# Constant
+PER_LINE = 5
+
+count = 0
+
+for i in range(1000, 1100):
+    if i % 11 == 0 or i % 17 == 0:
+        print(" ", i)
+        count += 1
+        if count % PER_LINE == 0:
+            count = 0
+            print("")
