8b973680a60ee5362c8da164ae9d207613ce5a4c
mehmetalikir <mehmet@senax.com.au>
2023-03-26 15:20:19 +1100

    Find the cheapest airline ticket

--- chapter05/Exercise05_10.py
+++ chapter05/Exercise05_10.py
@@ -6,3 +6,20 @@
 the number of airlines and each airline's name and ticket price. Find the airline
 with the cheapest ticket and display its name and price. Assume that the number
 of airlines is at least 1.'''
+
+# Prompts prompts the user to enter the number of airlines
+numberOfAirlines = int(input("Please enter the number of airlines: "))
+
+# Initialize variables
+highestName = 0
+highestPrice = 1000
+
+
+for i in range(numberOfAirlines):
+    airlineName = (input("Please enter an airline name: "))
+    ticketPrice = int(input("Please enter ticket price: "))
+    if ticketPrice < highestPrice:
+        highestName = airlineName
+        highestPrice = ticketPrice
+
+print(f"Cheapest airline {highestName}'s ticket price is {highestPrice}")
