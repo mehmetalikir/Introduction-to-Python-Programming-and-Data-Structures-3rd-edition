2d1a59308f1bd061132ebd69b7442789f3a33ca9
mehmetalikir <mehmet@senax.com.au>
2023-03-27 11:35:12 +1100

    Summation

--- chapter05/Exercise05_37.py
+++ chapter05/Exercise05_37.py
@@ -2,4 +2,17 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Summation) Write a program to compute the following summation.
+1 / 1 + 2√2 + 1 / 2√2 + 2√3 + 1 / 2√3 + 2√4 + ... + 1 / 2√624 + 2√625'''
+
+import math
+
+# Assign value
+sum = 0
+
+# Compute summation
+for i in range(1, 625, +1):
+    sum += 1 / (math.sqrt(i) + math.sqrt(i + 1))
+
+# Display result
+print("Summation: ", sum)
