54cdb1072538ae0bf2f1672c1ed46b4737cf884f
mehmetalikir <mehmet@senax.com.au>
2023-03-28 00:14:26 +1100

    Rebase main branch

--- chapter05/Exercise05_40.py
+++ chapter05/Exercise05_40.py
@@ -2,4 +2,5 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Simulation: heads or tails) Write a program that simulates flipping a coin one
+million times and displays the number of heads and tails.'''
