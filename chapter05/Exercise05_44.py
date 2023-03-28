72d7e052fe23581858f6e7720fa7783244e89774
mehmetalikir <mehmet@senax.com.au>
2023-03-29 02:46:16 +1100

    Decimal to binary

--- chapter05/Exercise05_44.py
+++ chapter05/Exercise05_44.py
@@ -2,4 +2,24 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Decimal to binary) Write a program that prompts the user to enter a decimal
+integer and displays its corresponding binary value.'''
+
+# Prompt the user to enter a decimal integer
+decimal = int(input("Enter a decimal integer: "))
+
+# Assign values
+toBinary = ""
+
+# Convert the decimal number to a binary number
+while decimal >= 1:
+    if decimal % 2 == 0:
+        toBinary = "0" + toBinary
+    else:
+        toBinary = "1" + toBinary
+
+    decimal /= 2
+
+# Display result
+print(toBinary)
+
