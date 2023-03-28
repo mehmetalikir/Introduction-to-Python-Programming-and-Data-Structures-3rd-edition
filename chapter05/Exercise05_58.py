5e3d594585d3a2587f911677d91d0e5398e8a0de
mehmetalikir <mehmet@senax.com.au>
2023-03-29 04:29:01 +1100

    Reverse a string

--- chapter05/Exercise05_58.py
+++ chapter05/Exercise05_58.py
@@ -2,4 +2,28 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Reverse a string) Write a program that prompts the user to enter a string and
+displays the string in reverse order.'''
+
+# Prompt the user to enter a string
+s1 = input("Please enter a string: ")
+
+# Assign values
+s2 = ""
+s3 = ""
+
+# Reverse the string f1
+for i in range(0,len(s1)):
+    s2 = s1[i] + s2
+
+# Reverse the string f2
+for i in range(len(s1) -1, -1, -1):
+    s3 += s1[i]
+
+# Display result
+print(s2)
+print(s3)
+
+
+
+
