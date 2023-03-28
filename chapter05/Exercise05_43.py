fd2e1a91d3084b189b4791a2e56ef443da8e93b8
mehmetalikir <mehmet@senax.com.au>
2023-03-29 02:30:39 +1100

    Math: combinations

--- chapter05/Exercise05_43.py
+++ chapter05/Exercise05_43.py
@@ -2,4 +2,20 @@
 # Email : hidirsezgin@gmail.com, mehmetalikir@outlook.com
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''' '''
+'''(Math: combinations) Write a program that displays all possible combinations
+for picking two numbers from integers 1 to 7. Also display the total number of
+all combinations.'''
+
+# Assign values
+number1 = 1
+number2 = 7
+count = 0
+
+# Pick two numbers from integers 1 to 7
+for number1 in range(number1, number2+1):
+    for number2 in range(number1+1, number2+1):
+        print(number1, number2)
+        count += 1
+
+# Display result
+print("Total number of all combinations: ", count)
