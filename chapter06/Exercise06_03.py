bf524ecb472a778abc7aac20e9b91033fa2f8e0d
mehmetalikir <mehmet@senax.com.au>
2023-04-01 02:44:23 +1100

    Palindrome integer

--- chapter06/Exercise06_03.py
+++ chapter06/Exercise06_03.py
@@ -2,8 +2,43 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
+'''(Palindrome integer) Write the methods with the following headers
 
-''''''
+Use the reverse method to implement isPalindrome. A number is a palindrome
+if its reversal is the same as itself. Write a test program that prompts the
+user to enter an integer and reports whether the integer is a palindrome.'''
 
 
+# Return the reversal of an integer, i.e. reverse(456) returns 654
+def reverse(number):
+    number = str(number)
+    reversed = ""
 
+    for i in range(0, len(number), +1):
+        reversed = number[i] + reversed
+
+    return reversed
+
+
+# Return true if number is a palindrome
+def isPalindrome(number):
+
+    # Check whether number is palindrome
+    if number == int(reverse(number)):
+        return True
+    else:
+        return False
+
+def main():
+
+    # Prompt the user enter an integer:
+    number = int(input("Please enter a positive integer: "))
+
+    # Display result
+    if isPalindrome(number):  # Invoke function
+        print(f"{number} is a palindrome")
+    else:
+        print(f"{number} is not a palindrome")
+
+
+main()
