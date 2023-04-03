e2eddf098487fa4499505d77909e9039338744b3
mehmetalikir <mehmet@senax.com.au>
2023-04-04 01:24:58 +1000

    Display pattern

--- chapter06/Exercise06_06.py
+++ chapter06/Exercise06_06.py
@@ -3,7 +3,32 @@
 # GitHub: /hidirsezgin, /mehmetalikir
 
 
-''''''
+'''(Display pattern) Write a function to display a pattern a follows:
+1
+2 1
+3 2 1
+4 3 2 1
+...
+The function header is
 
+
+Write a test program that prompts the user to enter a number n and invokes
+displayPattern(n) to display the pattern'''
+def displayPattern(n):
+
+    # Print triangle pattern
+    for i in range(1, n + 1):
+        for j in range(i, 0, -1):
+            print(j, end=" ")
+        print('')
+
+def main():
+
+    # Prompt the user to enter a number n
+    n = int(input("Please enter line number: "))
+
+    displayPattern(n) # Invoke function
+
+main()
 
 
