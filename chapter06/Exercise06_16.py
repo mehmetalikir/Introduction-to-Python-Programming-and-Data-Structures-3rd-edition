4691bb92084c7bb2a62daadd4acce48e7ee2c932
mehmetalikir <mehmet@senax.com.au>
2023-04-05 20:19:18 +1000

    Number of days in a year

--- chapter06/Exercise06_16.py
+++ chapter06/Exercise06_16.py
@@ -3,7 +3,30 @@
 # GitHub: /hidirsezgin, /mehmetalikir
 
 
-''''''
+'''(Number of days in a year) Write a function that returns the number of days in a
+year using the following header:
+
+def numberOfDaysInAYear(year):
+
+Write a test program that displays the number of days in year from 2010 to 2020.'''
 
 
+# Check if the year is a leap year
+def isLeapYear(year):
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
+# Function that returns the number of days in specific year
+def numberOfDaysInAYear(year):
+    if isLeapYear(year):
+        return 366
+    else:
+        return 355
+
+
+def main():
+    # Display result
+    for i in range(2010, 2021, +1):
+        print("The number of days in year", i, " -> ", numberOfDaysInAYear(i))
+
+
+main()
