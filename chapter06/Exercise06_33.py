870c54b3b98dcc6d40b003a2f76a13ab9684b9c1
mehmetalikir <mehmet@senax.com.au>
2023-04-08 13:10:31 +1000

    Geometry: area of a hexagon

--- chapter06/Exercise06_33.py
+++ chapter06/Exercise06_33.py
@@ -2,8 +2,24 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
+'''(Geometry: area of a hexagon) Write a program with function that return the area
+of hexagon. Prompt the user to enter the side of hexagon.
+((3√3 * s^2) / 2  )'''
+import math
 
-''''''
+
+# Function that return the area of hexagon
+def area(s):
+    return (3 * math.sqrt(3) *
+            (s * s)) / 2
 
 
+def main():
+    # Prompt the user to enter the side of hexagon.
+    s = int(input("Please enter the side of hexagon: "))
 
+    # Display result
+    print(f"Area of hexagon is %.2f " % area(s))
+
+
+main()
