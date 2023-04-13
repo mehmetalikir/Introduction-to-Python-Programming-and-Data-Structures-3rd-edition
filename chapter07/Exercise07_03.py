8467a2884550f2712d6d8180c3a1ec07080fde61
mehmetalikir <mehmet@senax.com.au>
2023-04-13 22:22:56 +1000

    Listing 7.6 CountLettersInList.py

--- chapter07/Exercise07_03.py
+++ chapter07/Exercise07_03.py
@@ -2,4 +2,13 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Count occurrence of numbers) Write a program that reads the integers between 1
+and 100 and counts the occurrences of each.
+
+Note that if a number occurs more than one time, the plural word "times" is used
+in the output'''
+
+# Prompt the user to enter integers between 1 and 100
+s = (input("Please enter the integers between 1 and 100: "))
+
+# Convert items to integers and create a list of numbers
