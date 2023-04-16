a70e65f86b6f9ca4e4efb0f847dbca3f83fda3ae
mehmetalikir <mehmet@senax.com.au>
2023-04-17 07:49:17 +1000

    Listing 5.14, PrimeNumber.py

---
+++ chapter07/Exercise07_06.py
@@ -0,0 +1,22 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+'''(Revise Listing 5.14, PrimeNumber.py) Listing 5.14 determines whether a number
+n is prime by checking whether 2, 3, 4, 5, 6, . . . , n/2 is a divisor. If a divisor
+is found, n is not prime. A more efficient approach is to check whether any of the
+prime numbers less than or equal to 2n can divide n evenly. If not, n is prime.
+Rewrite Listing 5.14 to display the first 50 prime numbers using this approach.
+You need to use a list to store the prime numbers and later use them to check
+whether they are possible divisors for n.'''
+
+
+
+
+
+
+
+
+
+
+
