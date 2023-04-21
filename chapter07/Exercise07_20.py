3eda7abd5a8796684f5bd807830498fc57c6a5e3
mehmetalikir <mehmet@senax.com.au>
2023-04-21 15:21:34 +1000

    Game: multiple Eight Queens solutions

--- chapter07/Exercise07_20.py
+++ chapter07/Exercise07_20.py
@@ -2,4 +2,8 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Game: multiple Eight Queens solutions) Programing Exercise 7.18 asks you
+to find one solution for the Eight Queens puzzle. Write a program to count all
+possible solutions for the Eight Queens problem and display all the solutions.'''
+
+# TODO
