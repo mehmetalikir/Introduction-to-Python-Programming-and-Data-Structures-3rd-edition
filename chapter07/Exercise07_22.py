798c3a953b676ceb819432fd465769b6ca78d940
mehmetalikir <mehmet@senax.com.au>
2023-04-22 16:42:23 +1000

    Listing 7.2 DeckOfCards.py

--- chapter07/Exercise07_22.py
+++ chapter07/Exercise07_22.py
@@ -2,4 +2,14 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Simulation: coupon collector’s problem) Coupon collector is a classic statistics
+problem with many practical applications. The problem is to pick objects from
+a set of objects repeatedly and find out how many picks are needed for all the
+objects to be picked at least once. A variation of the problem is to pick cards from
+a shuffled deck of 52 cards repeatedly and find out how many picks are needed
+before you see one of each suit. Assume a picked card is placed back in the deck
+before picking another. Write a program to simulate the number of picks needed
+to get four cards from each suit and display the four cards picked (it is possible a
+card may be picked twice)'''
+
+
