28262031835035e789b529a459e5c3fd263451dc
mehmetalikir <mehmet@senax.com.au>
2023-04-23 18:30:20 +1000

    Simulation: coupon collector’s problem :spades: :hearts: :diamonds: :clubs:

--- chapter07/Exercise07_22.py
+++ chapter07/Exercise07_22.py
@@ -12,4 +12,51 @@ before picking another. Write a program to simulate the number of picks needed
 to get four cards from each suit and display the four cards picked (it is possible a
 card may be picked twice)'''
 
+import random
 
+
+def main():
+    # Assign values
+    count = 0
+    picks = 0
+
+    # Create a list to hold each suit
+    suitCount = [0] * 4
+
+    # Simulate the number of picks needed to get four cards from each suit
+    while count < 4:
+        randomCard = getRandom()
+        picks += 1
+        if suitCount[randomCard // 13] == 0:
+            suitCount[randomCard // 13] += 1
+            count += 1
+
+            # Display the four cards picked
+            getCard(randomCard)
+
+    print("Number of picks: ", picks)
+
+
+def getCard(randomCard):
+    # Create a deck of cards
+    deck = [x for x in range(0, 52)]
+
+    # Create suits and ranks lists
+    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+             "10", "Jack", "Queen", "King"]
+
+    # Pick random card from deck
+    suit = suits[deck[randomCard] // 13]
+    rank = ranks[deck[randomCard] % 13]
+
+    print(rank, "of", suit)
+
+
+def getRandom():
+    return random.randint(0, 52)
+
+
+main()  # Invoke main function
+
+# TO-DO -> IndexError: list index out of range {suitCount[randomCard // 13] == 0}
