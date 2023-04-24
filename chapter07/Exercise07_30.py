8adf82476a0b6028eeeb8eed9a48d1c7465e74b2
mehmetalikir <mehmet@senax.com.au>
2023-04-24 19:19:51 +1000

    Culture: Chinese Zodiac :rabbit:

--- chapter07/Exercise07_30.py
+++ chapter07/Exercise07_30.py
@@ -2,4 +2,12 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Culture: Chinese Zodiac) Simplify Listing 3.4 ChineseZodiac.py, using a list of
+strings to store the animals' names.'''
+
+year = int(input("Please enter a year: "))
+
+months = ["Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox",
+          "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"]
+
+print(months[year % 12])
