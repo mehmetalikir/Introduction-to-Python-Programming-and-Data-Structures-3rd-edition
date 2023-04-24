7ca678ff856e9d60ef273b0e63c8efce9da92ab5
mehmetalikir <mehmet@senax.com.au>
2023-04-24 22:49:10 +1000

    Algebra: perfect square

--- chapter07/Exercise07_37.py
+++ chapter07/Exercise07_37.py
@@ -2,4 +2,13 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Algebra: perfect square) Write a program that prompts the user to enter and integer
+m ad find the smallest integer n such that m * n is a perfect square. (Hint:
+Store all smallest factors of m into a list. n is the product of the factors that appear
+an odd number of times in the list. For example, consider m = 90, store the
+factors 2, 3, 3, 5 in a lsit. 2 and 5 appear an odd number of times in the vector.
+So, n is 10)'''
+
+
+
+
