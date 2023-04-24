0a181199f6e07524e5d9a8b2b7385b92e4d1fc83
mehmetalikir <mehmet@senax.com.au>
2023-04-24 23:10:21 +1000

    :mortar_board: TODO :x:

---
+++ chapter08/Exercise08_31.py
@@ -0,0 +1,16 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+''''''
+
+
+def main():
+    pass
+
+
+def getIt():
+    pass
+
+
+main()  # Invoke main function
