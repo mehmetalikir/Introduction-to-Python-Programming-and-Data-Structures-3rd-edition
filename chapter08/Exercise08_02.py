6e7f7cca62bc5938c028ff36b549a4228c49f23b
mehmetalikir <mehmet@senax.com.au>
2023-04-26 21:52:03 +1000

    Average the major diagonal in a matrix

--- chapter08/Exercise08_02.py
+++ chapter08/Exercise08_02.py
@@ -2,15 +2,62 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Average the major diagonal in a matrix) Write a function that finds average of
+the numbers of the major diagonal in an n x n matrix of integers using the
+following header:
+
+    def avgMajorDiagonal(m):
+
+The major diagonal is the diagonal that runs from the top left corner to the bottom
+right corner in the square matrix. Write a test program that reads a 4 x 4 matrix and
+displays the average of elements on the major diagonal.
+7 9 5 2
+8 3 7 0
+2 6 1 7
+3 9 7 5'''
 
 
 def main():
-    pass
+    m = getMatrix()  # Get a list
+
+    # Display the average of elements on the major diagonal
+    print(f"Average of the elements in the "
+          f"major diagonal is {avgMajorDiagonal(m)}")
 
 
-def getIt():
-    pass
+# Get average of the numbers
+def avgMajorDiagonal(m):
+    # Assign value
+    sum = 0.0
+    numbers = 0
+
+    # Find the numbers of the major diagonal in matrix
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            numbers = len(m[i])
+            if i == j:
+                sum += m[j][i]
+
+    return sum / numbers
+
+
+def getMatrix():
+    # Create an empty list
+    matrix = []
+
+    # Assign values
+    numberOfRows = 4
+    numberOfColumns = 4
+
+    # Prompt the user to enter values
+    for row in range(numberOfRows):
+        matrix.append([])
+        s = (input(f"Please enter a 4-by-4 matrix row {row}: "))
+        for column in range(numberOfColumns):
+            value = [float(x) for x in s.split()]
+            matrix[row].append(value[column])
+
+    return matrix
 
 
 main()  # Invoke main function
