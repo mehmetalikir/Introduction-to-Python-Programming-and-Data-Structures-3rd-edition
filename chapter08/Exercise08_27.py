2ef213befc33121118c8383bc75a618a47f776e1
mehmetalikir <mehmet@senax.com.au>
2023-05-01 07:01:43 +1000

    Column sorting :panda_face:s

--- chapter08/Exercise08_27.py
+++ chapter08/Exercise08_27.py
@@ -12,11 +12,37 @@ displays a new column-sorted matrix.'''
 
 
 def main():
-    pass
+    # m = [[1, 4, 8],
+    #      [7, 9, 0],
+    #      [5, 2, 6]]
+
+    SIZE = 3
+    print("Enter a 3 by 3 matrix row by row: ")
+    m = []
+
+    for i in range(SIZE):
+        line = input().split()
+        m.append([float(x) for x in line])
+
+    # Display a new column-sorted matrix
+    print("The column-sorted list is ")
+    printMatrix(sortColumns(m))
 
 
-def getIt():
-    pass
+def printMatrix(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            print(m[j][i], end=" ")
+        print()
+
+
+def sortColumns(m):
+    import pandas as pd
+    df = pd.DataFrame(data=m)
+    for col in df:
+        df[col] = df[col].sort_values(ignore_index=True)  # (by= df.columns[0])
+
+    return df
 
 
 main()  # Invoke main function
