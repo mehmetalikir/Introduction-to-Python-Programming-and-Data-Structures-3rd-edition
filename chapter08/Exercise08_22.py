b6db2d08944160e7f4a201407d6005f78e607794
mehmetalikir <mehmet@senax.com.au>
2023-04-30 17:51:00 +1000

    Even number of 1s

--- chapter08/Exercise08_22.py
+++ chapter08/Exercise08_22.py
@@ -6,13 +6,44 @@
 matrix filled with 0s and 1s, displays the matrix, and checks to see if every row
 and every column has an even number of 1s.'''
 
+import random
+
 
 def main():
-    pass
+    n = 6
+    matrix = []
+
+    # Initialize matrix and display it
+    for i in range(n):
+        matrix.append([])
+        for j in range(n):
+            matrix[i].append(random.randint(0, 1))
+            print(matrix[i][j], end=" ")
+
+        print()
+
+    if isEvenParity(matrix):
+        print("All rows and columns are even")
+    else:
+        print("Not all rows and columns are even")
 
 
-def getIt():
-    pass
+def isEvenParity(matrix):
+    for i in range(len(matrix)):
+        sum = 0
+        for j in range(len(matrix[i])):
+            sum += matrix[i][j]
+        if sum % 2 != 0:
+            return False
+
+    for j in range(len(matrix[0])):
+        sum = 0
+        for i in range(len(matrix)):
+            sum += matrix[i][j]
+        if sum % 2 != 0:
+            return False
+
+    return True
 
 
 main()  # Invoke main function
