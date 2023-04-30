8e066d3126561e7f18733c427dba7267297d7073
mehmetalikir <mehmet@senax.com.au>
2023-04-30 17:54:39 +1000

    Row sorting

--- chapter08/Exercise08_26.py
+++ chapter08/Exercise08_26.py
@@ -12,11 +12,34 @@ displays a new row-sorted matrix.'''
 
 
 def main():
-    pass
+    SIZE = 3
+    print("Enter a 3 by 3 matrix row by row: ")
+    m = []
+
+    for i in range(SIZE):
+        line = input().split()
+        m.append([float(x) for x in line])
+
+    print("The row-sorted list is ")
+    printMatrix(sortRows(m))
 
 
-def getIt():
-    pass
+def printMatrix(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            print(m[i][j], end=" ")
+        print()
+
+
+def sortRows(m):
+    result = []
+    for row in m:
+        result.append(row)
+
+    for row in result:
+        row.sort()
+
+    return result
 
 
 main()  # Invoke main function
