58900d741e9051ceff3bc6523dae304b1b445a5e
mehmetalikir <mehmet@senax.com.au>
2023-05-01 02:26:20 +1000

    Game: find the flipped cell :relaxed:

--- chapter08/Exercise08_23.py
+++ chapter08/Exercise08_23.py
@@ -10,13 +10,82 @@ find which cell was flipped. Your program should prompt the user to enter a
 column c where the even number of 1s property is violated. The flipped cell
 is at (r, c).'''
 
+import random
+
 
 def main():
-    pass
+    # Create a list
+    grid = createList()
+
+    if isEvenParity(grid):
+        print("All rows and columns are even")
+        print("-----------------------------")
+        flippedGrid = readAGrid()
+        LocateFlippedCell(grid, flippedGrid)
+    else:
+        print("Not all rows and columns are even")
 
 
-def getIt():
-    pass
+def createList():
+    # Initialize matrix
+    grid = [[1, 1, 1, 1, 0, 0],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 0, 1],
+            [0, 1, 1, 1, 1, 0],
+            [1, 0, 0, 1, 0, 0]]
+
+    return grid
+
+
+def isEvenParity(grid):
+    for i in range(len(grid)):
+        sum = 0
+        for j in range(len(grid[i])):
+            sum += grid[i][j]
+        if sum % 2 != 0:
+            return False
+
+    for j in range(len(grid[0])):
+        sum = 0
+        for i in range(len(grid)):
+            sum += grid[i][j]
+        if sum % 2 != 0:
+            return False
+
+    return True
+
+
+def LocateFlippedCell(grid, flippedGrid):
+    # Assign values
+    indexRow = 0
+    indexCol = 0
+
+    # Locate where the even number of 1s property is violated
+    for i in range(len(grid[0])):
+        for j in range(len(grid[0])):
+            if grid[i][j] != flippedGrid[i][j]:
+                indexRow = i
+                indexCol = j
+                break
+
+    print(f"The first row and column where the parity violated is at ({indexRow},{indexCol})")
+
+
+# Read a 6 x 6 two-dimensional list with 0s and 1s
+def readAGrid():
+    # 1 1 1 1 0 0
+    # 1 0 0 0 0 1
+    # 1 0 0 0 1 0
+    # 0 0 0 1 1 1
+    # 0 1 1 1 1 0
+    # 1 0 0 1 0 0
+    print("PLease enter a 6 x 6 matrix:")
+    grid = []
+    for i in range(6):
+        line = input().split()
+        grid.append([int(x) for x in line])
+    return grid
 
 
 main()  # Invoke main function
