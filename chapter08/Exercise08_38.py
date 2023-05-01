afa0de3bf15c537a1fd131c2a21d1bfe10eeb87a
mehmetalikir <mehmet@senax.com.au>
2023-05-02 08:16:44 +1000

    :turtle::draw a polygon/polyline

--- chapter08/Exercise08_38.py
+++ chapter08/Exercise08_38.py
@@ -3,14 +3,22 @@
 # GitHub: /hidirsezgin, /mehmetalikir
 
 ''''''
+import turtle
+from UsefulTurtleFunctions import drawLine
 
+# Draw a polyline to connect all the points in the list
+def drawPolyline(points):
+    for i in range(len(points) - 1):
+        drawLine(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
 
-def main():
-    pass
+# Draw a polygon to connect all the points in the list and
+# close the polygon by connecting the first point with the last point
+def drawPolygon(points):
+    drawPolyline(points)
+    drawLine(points[len(points) - 1][0], points[len(points) - 1][1], points[0][0], points[0][1]) # Close the polygon
 
-
-def getIt():
-    pass
-
-
-main()  # Invoke main function
+# Fill a polygon by connecting all the points in the list
+def fillPolygon(points):
+    turtle.begin_fill()
+    drawPolygon(points)
+    turtle.end_fill()
