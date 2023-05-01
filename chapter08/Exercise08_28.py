241301da89c7c176399b543a3072c9cd819d04ae
mehmetalikir <mehmet@senax.com.au>
2023-05-01 20:08:26 +1000

    Check for square of list

--- chapter08/Exercise08_28.py
+++ chapter08/Exercise08_28.py
@@ -2,15 +2,33 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Check for square of list) The lsit m2 is the square of the list m1 if each element
+of m2 is equal to the square of the corresponding element in m1. Write a function
+that returns True if all the elements of m2 are square of the corresponding elements
+of m1, using the following header:
+
+    def isSquare(m1, m2):
+
+Write a test program that prompts the user two lists of integers and display
+whether the second list is square of first or not.'''
+
 
 
 def main():
-    pass
+    m1 = input("List1: please enter numbers: ").split() # 2 3 5 7 9
+    m2 = input("List2, please enter squares of list1: ").split() # 4 9 25 49 81
+
+    if isSquare(m1, m2):
+        print("list2 is square of list1")
+    else:
+        print("list2 is not square of list1")
 
 
-def getIt():
-    pass
+def isSquare(m1, m2):
+    for i in range(len(m1)):
+        if int(m2[i]) != int(m1[i]) * int(m1[i]): return False
+
+    return True
 
 
 main()  # Invoke main function
