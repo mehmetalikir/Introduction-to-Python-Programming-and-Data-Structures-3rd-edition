369879c156950145c82214f2483002f807768786
mehmetalikir <mehmet@senax.com.au>
2023-05-05 08:19:08 +1000

    Use the Rational class

--- chapter09/Exercise09_14.py
+++ chapter09/Exercise09_14.py
@@ -2,5 +2,17 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Use the Rational class) Write a program that computes the following summation
+series using the Rational class:'''
+
+from Rational import Rational
+
+sum = Rational(0, 1)
+
+for i in range(1, 10):
+    sum = sum + Rational(i, i + 1)
+
+# Display results
+print("Sum is " + str(sum))
+
 
