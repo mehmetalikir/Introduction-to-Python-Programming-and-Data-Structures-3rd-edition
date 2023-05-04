ecd64f263fa67fe1104622ac3847a19a9ee66b6c
mehmetalikir <mehmet@senax.com.au>
2023-05-05 09:31:51 +1000

    Convert decimals to fractions

--- chapter09/Exercise09_16.py
+++ chapter09/Exercise09_16.py
@@ -2,5 +2,21 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Convert decimals to fractions) Write a program that prompts the user to enter
+a decimal number and displays the number in a fraction. Hint: Read the decimal
+number as a string, extract the integer part and fractional part from the string,
+and use the Rational class to obtain a rational number for the decimal number.'''
 
+import Rational
+
+
+def getFraction(decimal):
+    items = decimal.split(".")
+    if len(items) <= 1:
+        return decimal
+    else:
+        return Rational.Rational(int(items[0] + items[1]), 10 ** len(items[1]))
+
+
+decimal = input("Please enter a decimal number: ")
+print("The fraction number is", getFraction(decimal))
