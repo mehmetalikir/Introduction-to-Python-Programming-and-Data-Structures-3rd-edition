f5dcd6cc0e74dd661a48c7ebd8fbc2d9356b4db0
mehmetalikir <mehmet@senax.com.au>
2023-05-07 13:14:34 +1000

    :mortar_board: TODO :x:

---
+++ chapter10/Exercise10_25.py
@@ -0,0 +1,18 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+''''''
+
+from tkinter import *  # Import tkinter
+
+
+class MainGUI:
+    
+    def __init__(self):
+        window = Tk()  # Create a window
+
+        window.mainloop()  # Create an event loop
+
+
+MainGUI()  # Call the main function
