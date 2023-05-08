542438cff614a09aa426b7f3208b40193405b647
mehmetalikir <mehmet@senax.com.au>
2023-05-08 14:31:21 +1000

    Display old TV bars

--- chapter10/Exercise10_07.py
+++ chapter10/Exercise10_07.py
@@ -2,17 +2,33 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Display old TV bars) Write a program that displays 7 vertical-colored bars as
+shown in Figure 10.19c. Use white, yellow, cyan, green, magenta, red, and blue
+colors for bars'''
 
 from tkinter import *  # Import tkinter
 
 
-class MainGUI:
-    
+class TVBars:
     def __init__(self):
         window = Tk()  # Create a window
+        window.title("Old TV Bars")  # Set a title
+
+        frame = Frame(window)
+        frame.pack()
+
+        canvas = Canvas(frame, width=400, height=400, bg="white")
+        canvas.pack()
+
+
+        for i in range(8):
+            canvas.create_line(16 * i, 0 , 16 * i, 128, fill="red")
+            for j in range(8):
+                canvas.create_line(0, 16 * j, 128, 16 * j, fill="blue")
+
+
 
         window.mainloop()  # Create an event loop
 
 
-MainGUI()  # Call the main function
+TVBars()  # Create GUI
