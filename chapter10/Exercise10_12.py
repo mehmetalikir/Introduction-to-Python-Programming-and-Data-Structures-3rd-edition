c968e7a2f60ca6ad64324ec8a7e0c7bae9c123a3
mehmetalikir <mehmet@senax.com.au>
2023-05-09 07:09:47 +1000

    Display a still fan

--- chapter10/Exercise10_12.py
+++ chapter10/Exercise10_12.py
@@ -2,17 +2,31 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
-
+'''(Display a still fan) Write a program that displays a still fan, as shown in Figure
+10.21b'''
 from tkinter import *  # Import tkinter
 
 
 class MainGUI:
-    
     def __init__(self):
         window = Tk()  # Create a window
+        window.title("Fan")  # Set a title
+
+        # Constant
+        WIDTH = 200
+        HEIGHT = 200
+        RADIUS = 80
+
+        canvas = Canvas(window, bg="white", width=WIDTH, height=HEIGHT)
+        canvas.pack()
+
+        angle = 0
+        while angle <= 270:
+            canvas.create_arc(WIDTH / 2 - RADIUS, HEIGHT / 2 - RADIUS, WIDTH / 2 + RADIUS, HEIGHT / 2 + RADIUS,
+                              start=angle, extent=30, fill="red", tags="fan")
+            angle += 90
 
         window.mainloop()  # Create an event loop
 
 
-MainGUI()  # Call the main function
+MainGUI()  # Create GUI
