56158127454d563d8ad1f508c3d3d605c346252b
mehmetalikir <mehmet@senax.com.au>
2023-05-09 10:07:19 +1000

    Display balls with random colors

--- chapter10/Exercise10_15.py
+++ chapter10/Exercise10_15.py
@@ -2,17 +2,39 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
-
+'''(Display balls with random colors) Write a program that displays ten balls with
+random colors and placed at random locations, at shown in Figure 10.22c.'''
+import random
 from tkinter import *  # Import tkinter
+
+WIDTH = 200
+HEIGHT = 150
 
 
 class MainGUI:
-    
     def __init__(self):
         window = Tk()  # Create a window
+        window.title("Random Balls")  # Set title
+
+        self.canvas = Canvas(window, bg="white", width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+        frame1 = Frame(window)
+        frame1.pack()
+
+        Button(frame1, text="Display", command=self.displayFigure).pack(side=BOTTOM)
 
         window.mainloop()  # Create an event loop
 
+    def displayFigure(self):
+        x = random.randrange(0, WIDTH)
+        y = random.randrange(0, HEIGHT)
+        self.canvas.create_oval(x, x, y, y, fill=self.randomColor())
 
-MainGUI()  # Call the main function
+    def randomColor(self):
+        colors = ["red", "blue", "yellow", "green"]
+        i = random.randint(0, len(colors))
+        return colors[i]
+
+
+MainGUI()  # Create GUI
