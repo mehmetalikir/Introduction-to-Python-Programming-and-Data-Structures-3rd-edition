75d9f275fbc394da49e5e48526b0fd19b09d8ace
mehmetalikir <mehmet@senax.com.au>
2023-05-12 09:09:18 +1000

    Change font type

--- chapter11/Exercise11_02.py
+++ chapter11/Exercise11_02.py
@@ -2,34 +2,33 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
-
+'''(Change font type) Write a program that contains a combo box and a label. The
+combo box shows five strings: Arial, Verdana, Helvetica, Times, and Courier.
+The label displays the string: "Programing is fun" with the specified font type
+from the combo box, as shown in Figure 11.14'''
 from tkinter import *  # Import tkinter
 
 
-class MainGUI:
+class ChangeFontType:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Set Font Type")  # Set title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
-        self.canvas.pack()
+        # Create a combo box to select font
+        var = StringVar()
+        var.set("Arial")  # initial value
+        comboBox = OptionMenu(window, var, "Arial", "Times", "Courier",
+                              command=self.processSelection).pack(fill=BOTH)
 
-        # Create object
-        self.object = self.canvas.create_object
-
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
-
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        self.label = Label(window, text="Programming is fun", font="Arial")
+        self.label["font"] = "Arial"
+        self.label.pack()
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    # Display text as per selected font
+    def processSelection(self, selectedItem):
+        self.label["font"] = selectedItem
 
 
-MainGUI()  # Call the main function
+ChangeFontType()  # Create GUI
