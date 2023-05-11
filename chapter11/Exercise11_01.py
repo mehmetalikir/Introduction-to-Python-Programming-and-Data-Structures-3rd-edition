bf5e32aeb78d4eba91c737d130c70882de14b542
mehmetalikir <mehmet@senax.com.au>
2023-05-12 09:03:54 +1000

    Display selected sized shape

--- chapter11/Exercise11_01.py
+++ chapter11/Exercise11_01.py
@@ -2,34 +2,40 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Display selected sized shape) Write a program that contains a combo box and
+a canvas. The combo box show three strings: Small, Medium and Large. The canvas
+displays a shape with size according to the selection in the combo box, as shown in
+Figure 11.13'''
 
 from tkinter import *  # Import tkinter
 
 
-class MainGUI:
+class DisplayShape:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Select Shapes")  # Set title
+
+        # Create a combo box to select shape
+        var = StringVar()
+        var.set("rectangle")  # initial value
+        comboBox = OptionMenu(window, var, "rectangle", "oval", "arc",
+                              command=self.processSelection).pack(fill=BOTH)
 
         # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
+        self.canvas = Canvas(window, width=200, height=100, bg="white")
         self.canvas.pack()
-
-        # Create object
-        self.object = self.canvas.create_object
-
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
-
-        Button(frame, text="", command=self.function).pack(side=LEFT)
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    # Display text as per selected font
+    def processSelection(self, selectedItem):
+        self.canvas.delete("shapes")
+        if selectedItem == "rectangle":
+            self.canvas.create_rectangle(10, 10, 190, 90, fill="", tags="shapes")
+        elif selectedItem == "oval":
+            self.canvas.create_oval(10, 10, 190, 90, fill="red", tags="shapes")
+        else:
+            self.canvas.create_arc(10, 10, 190, 90, fill="red", tags="shapes")
 
 
-MainGUI()  # Call the main function
+DisplayShape()  # Create GUI
