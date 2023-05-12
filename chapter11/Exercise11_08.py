31fbe225a453c6f343847791af6486f5cc87dd53
mehmetalikir <mehmet@senax.com.au>
2023-05-12 13:32:06 +1000

    Display animated text

--- chapter11/Exercise11_08.py
+++ chapter11/Exercise11_08.py
@@ -2,34 +2,50 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Display animated text) Write a program that displays the animated text “Welcome
+to Python Programing” word by word on screen. Display only one word at a time on the
+screen, remove it, display the next word and so on, as shown in Figure 11.18a–b.
+(Hint: Use a numeric variable to control the alternation'''
 
 from tkinter import *  # Import tkinter
+
+width = 200
+height = 100
 
 
 class MainGUI:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Display animated text")  # Set a title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
-        self.canvas.pack()
+        sleepTime = 400
+        canvas = Canvas(window, bg="white", width=width, height=height)
+        canvas.pack()
 
-        # Create object
-        self.object = self.canvas.create_object
+        count = 0  # To control the alteration
+        while True:
+            if count == 0:
+                canvas.delete("text")
+                canvas.create_text(width / 2, height / 2, text="Welcome", tags="text")
+                count += 1
+            elif count == 1:
+                canvas.delete("text")
+                canvas.create_text(width / 2, height / 2, text="To", tags="text")
+                count += 1
+            elif count == 2:
+                canvas.delete("text")
+                canvas.create_text(width / 2, height / 2, text="Python", tags="text")
+                count += 1
+            elif count == 3:
+                canvas.delete("text")
+                canvas.create_text(width / 2, height / 2, text="Programming", tags="text")
 
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
+                count = 0  # reset count
 
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+            canvas.after(sleepTime)  # Sleep for 400 milliseconds
+            canvas.update()
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
 
-
-MainGUI()  # Call the main function
+MainGUI()
