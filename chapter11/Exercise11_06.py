a06cd33db17d45753e52b3b0678688ba62f54275
mehmetalikir <mehmet@senax.com.au>
2023-05-12 11:57:47 +1000

    Display a running fan

--- chapter11/Exercise11_06.py
+++ chapter11/Exercise11_06.py
@@ -2,34 +2,55 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Display a running fan) Write a program that displays a fan running, as shown in
+Figure 11.17a'''
 
 from tkinter import *  # Import tkinter
 
+width = 200
+height = 200
+radius = 80
 
-class MainGUI:
+
+class ARunningFan:
     def __init__(self):
-        window = Tk()  # Create a window
-        window.title("")  # Set title
+        self.window = Tk()  # Create a window
+        self.window.title("Fan")  # Set a title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
+        self.canvas = Canvas(self.window, bg="white", width=width, height=height)
         self.canvas.pack()
 
-        # Create object
-        self.object = self.canvas.create_object
+        self.window.protocol("WM_DELETE_WINDOW", self.quit)  # Handle window closed event
 
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
+        self.startingAngle = 0
+        self.isContinue = True
+        while self.isContinue:  # Exit when window is closed
+            self.startingAngle += 5
+            self.displayFan(self.startingAngle)
+            self.canvas.after(100)  # Sleep for 100 milliseconds
+            self.canvas.update()
 
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        self.window.mainloop()  # Create an event loop
 
-        window.mainloop()  # Create an event loop
+    def displayFan(self, startingAngle):
+        self.canvas.delete("fan")
 
-    def function(self):
-        self.canvas.does()
+        self.canvas.create_arc(width / 2 - radius, height / 2 - radius, width / 2 + radius, height / 2 + radius,
+                               start=self.startingAngle + 0, extent=30, fill="red", tags="fan")
+
+        self.canvas.create_arc(width / 2 - radius, height / 2 - radius, width / 2 + radius, height / 2 + radius,
+                               start=self.startingAngle + 90, extent=30, fill="red", tags="fan")
+
+        self.canvas.create_arc(width / 2 - radius, height / 2 - radius, width / 2 + radius, height / 2 + radius,
+                               start=self.startingAngle + 180, extent=30, fill="red", tags="fan")
+
+        self.canvas.create_arc(width / 2 - radius, height / 2 - radius, width / 2 + radius, height / 2 + radius,
+                               start=self.startingAngle + 270, extent=30, fill="red", tags="fan")
+
+    def quit(self):
+        self.isContinue = False
+        self.window.destroy()
 
 
-MainGUI()  # Call the main function
+ARunningFan()
+
