24243f9f3d0a3588a2bf27853366ad7198d4d7ad
mehmetalikir <mehmet@senax.com.au>
2023-05-12 13:40:08 +1000

    Move a circle using keys

--- chapter11/Exercise11_09.py
+++ chapter11/Exercise11_09.py
@@ -2,34 +2,40 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Move a circle using keys) Write a program that moves a circle up, down, left, or
+right, using the arrow keys, as shown in Figure 11.18c-d.'''
 
 from tkinter import *  # Import tkinter
 
 
-class MainGUI:
+class MoveACircle:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
-
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
+        window.title("Moving Circle")  # Set a title
+        self.canvas = Canvas(window, bg="white", width=250, height=250)
         self.canvas.pack()
 
-        # Create object
-        self.object = self.canvas.create_object
+        self.canvas.create_oval(25, 5, 75, 55, tags="circle")
 
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
-
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        # Bind with <Key> event
+        self.canvas.bind("<Key>", self.processKeyEvent)
+        self.canvas.focus_set()
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    def processKeyEvent(self, event):
+        if event.keysym == "Left":
+            self.canvas.move("circle", -20, 0)  # Move circle
+            self.canvas.update()  # Update canvas
+        if event.keysym == "Right":
+            self.canvas.move("circle", 20, 0)  # Move circle
+            self.canvas.update()  # Update canvas
+        if event.keysym == "Up":
+            self.canvas.move("circle", 0, -20)  # Move circle
+            self.canvas.update()  # Update canvas
+        if event.keysym == "Down":
+            self.canvas.move("circle", 0, 20)  # Move circle
+            self.canvas.update()  # Update canvas
 
 
-MainGUI()  # Call the main function
+MoveACircle()  # Create GUI
