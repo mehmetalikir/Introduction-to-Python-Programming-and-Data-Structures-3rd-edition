e5f7aaa00582d156b99c0b49e49900d425a03679
mehmetalikir <mehmet@senax.com.au>
2023-05-12 10:18:05 +1000

    Swap background and foreground color

--- chapter11/Exercise11_03.py
+++ chapter11/Exercise11_03.py
@@ -2,34 +2,45 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
-
+'''(Swap background and foreground color) Write a program to swap between
+background and foreground color of a canvas with text "Programming is fun"
+using a left mouse click, as shown in Figure 11.15'''
+import random
 from tkinter import *  # Import tkinter
 
+width = 220
+height = 100
 
-class MainGUI:
+
+class SwapColor:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Draw Dots")  # Set a title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
+        self.canvas = Canvas(window, bg="white", width=width, height=height)
         self.canvas.pack()
 
-        # Create object
-        self.object = self.canvas.create_object
-
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
-
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        # Bind canvas with mouse events
+        self.canvas.bind("<Button-1>", self.displayPosition)
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    def displayPosition(self, event):
+        if event:
+            self.canvas.configure(bg=self.getRandomColor())
+            self.canvas.create_text(110, 40, text="Programing is Fun", fill=self.getRandomColor(), font="Times 11 bold")
+
+    def getRandomColor(self):
+        color = "#"
+        for r in range(6):
+            color += self.toHexChar(random.randint(0, 15))
+        return color
+
+    def toHexChar(self, hexValue):
+        if 0 <= hexValue <= 9:
+            return chr(hexValue + ord('0'))
+        else:
+            return chr(hexValue - 10 + ord('A'))
 
 
-MainGUI()  # Call the main function
+SwapColor()
