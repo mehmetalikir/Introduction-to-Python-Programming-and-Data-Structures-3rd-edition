f737c7b47431167936489de40f9bcf0c9e0d1346
mehmetalikir <mehmet@senax.com.au>
2023-05-12 14:16:05 +1000

    Geometry: inside a rectangle:question:

--- chapter11/Exercise11_11.py
+++ chapter11/Exercise11_11.py
@@ -2,34 +2,52 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
-
+'''(Geometry: inside a rectangle?) Write a program that draws a fixed rectangle centered
+at (100, 60) with width 100 and height 40. Whenever the mouse is moved,
+display the message indicating whether the mouse pointer is inside the rectangle,
+as shown in Figure 11.20. To detect whether the pointer is inside a rectangle, use the
+Rectangle2D class defined in Exercise 9.13.'''
+import math
 from tkinter import *  # Import tkinter
+
+width = 240
+height = 120
+
+
+# hipotenus = 0
 
 
 class MainGUI:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Inside the rectangle?")  # Set a title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
+        self.canvas = Canvas(window, bg="white", width=width, height=height)
         self.canvas.pack()
 
-        # Create object
-        self.object = self.canvas.create_object
+        self.canvas.create_rectangle(10, 10, 100, 60, tags="rectangle")
 
-        # Place buttons in frame
-        frame = Frame(window)
-        frame.pack()
-
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        self.canvas.bind("<B1-Motion>", self.isInside)
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    def isInside(self, event):
+        self.canvas.delete("text")
+        if isInsideRectangle(100, 60, event.x, event.y):
+            self.canvas.create_text(event.x, event.y,
+                                    text="Mouse pointer is in the rectangle", tags="text")
+        else:
+            self.canvas.create_text(event.x, event.y,
+                                    text="Mouse pointer is not in the rectangle", tags="text")
 
 
-MainGUI()  # Call the main function
+def isInsideRectangle(r1X, r1Y, r2X, r2Y):
+    # Determine whether the mouse pointer is inside the rectangle
+    if ((math.pow(math.pow(r2Y - r1Y, 2), .05) + r2Y / 2 <= r1Y / 2) and
+            (math.pow(math.pow(r2X - r1X, 2), .05) + r2X / 2 <= r1X / 2) and
+            (r1Y / 2 + r2Y / 2 <= r1Y) and
+            (r1X / 2 + r2X / 2 <= r1X)):
+        return True
+
+
+MainGUI()
