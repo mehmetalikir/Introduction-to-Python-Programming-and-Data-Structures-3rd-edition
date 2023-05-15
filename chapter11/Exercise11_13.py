70996a1aa1a3255fe17e459f95b533f11e877e65
mehmetalikir <mehmet@senax.com.au>
2023-05-15 16:06:32 +1000

    Display circles

--- chapter11/Exercise11_13.py
+++ chapter11/Exercise11_13.py
@@ -2,34 +2,67 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-'''() Write a program that'''
+'''(Display circles) Write a program that displays a new larger circle with a left
+mouse click and removes the largest circle with a right mouse click, as shown in
+Figure 11.22.'''
 
 from tkinter import *  # Import tkinter
 
 
-class MainGUI:
+class Circle:
+    def __init__(self):
+        self.i = 3
+        self.x0 = 190 - self.i * 5
+        self.y0 = 110 - self.i * 5
+        self.x1 = 220 + self.i * 5
+        self.y1 = 140 + self.i * 5
+        self.outline = "#000"
+        self.width = 1.5
+
+
+class DisplayCircles:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("")  # Set title
+        window.title("Display Circles")  # Set a title
 
-        # Place self.canvas in the window
-        self.canvas = Canvas(window, width=400, height=200,
-                             bg="white")
-        self.canvas.pack()
-
-        # Create object
-        self.object = self.canvas.create_object
-
-        # Place buttons in frame
         frame = Frame(window)
         frame.pack()
 
-        Button(frame, text="", command=self.function).pack(side=LEFT)
+        # Place self.canvas in the window
+        self.canvas = Canvas(frame, width=400, height=400, bg="white")
+        self.canvas.pack()
+
+        self.circleList = []  # Create a list for circles
+
+        # Bind Button-1 displays a new larger circle with a left mouse click
+
+        # self.canvas.bind("<Button-1>", self.addCircle())
+        self.animate()
+
+        # # Bind Button-3 removes the largest circle with a right mouse click
+        # self.canvas.bind("<Button-3>", self.removeCircle)
 
         window.mainloop()  # Create an event loop
 
-    def function(self):
-        self.canvas.does()
+    # Display circles
+    def displayCricles(self, circle):
+        self.canvas.create_oval(circle.x0, circle.y0, circle.x1, circle.y1,
+                                outline=circle.outline, width=circle.width)
+
+    def animate(self):  # Move the message
+
+        for circle in self.circleList:
+            self.displayCricles(circle)
+
+        self.canvas.update()  # Update self.canvas
+
+    def addCircle(self, circle):  # Add a new circle
+
+        self.circleList.append(Circle())
+
+    def removeCircle(self):  # Remove the last circle
+
+        self.circleList.pop()
 
 
-MainGUI()  # Call the main function
+DisplayCircles()  # Create GUI
