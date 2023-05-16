578240ea687cfe628f9c0e82e96f5a70584e4d04
mehmetalikir <mehmet@senax.com.au>
2023-05-16 21:55:16 +1000

    Game: Tic-tac-toe

--- chapter12/Exercise12_05.py
+++ chapter12/Exercise12_05.py
@@ -4,3 +4,27 @@
 
 ''''''
 
+from random import *  # Import random
+from tkinter import *  # Import tkinter
+
+
+class MainGUI:
+    def __init__(self):
+        window = Tk()  # Create a window
+        window.title("TicTacToe")  # Set a title
+
+        # Assign values
+        imageX = PhotoImage(file="image/x.gif")
+        imageO = PhotoImage(file="image/o.gif")
+
+        for i in range(3):
+            frame = Frame(window)
+            frame.pack()
+            for j in range(3):
+                label = Label(frame, image=imageX if randrange(0, 2) == 0 else imageO)
+                label.pack(side=LEFT)
+
+        window.mainloop()  # Create an event loop
+
+
+MainGUI()  # Create GUI
