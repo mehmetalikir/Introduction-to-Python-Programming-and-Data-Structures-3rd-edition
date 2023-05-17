bae2902e265f0279d4d753dbdd34059892c622f9
mehmetalikir <mehmet@senax.com.au>
2023-05-17 13:30:29 +1000

    Tkinter: a group of clocks

--- chapter12/Exercise12_12.py
+++ chapter12/Exercise12_12.py
@@ -2,5 +2,26 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Tkinter: a group of clocks) Write a program that displays four clocks, as shown in
+Figure 12.24.'''
+
+from tkinter import *  # Import tkinter
+from StillClock import StillClock
+
+window = Tk()  # Create a window
+window.title("Display Clocks")  # Set title
+
+clock1 = StillClock(window)
+clock1.grid(row=1, column=1)
+
+clock2 = StillClock(window)
+clock2.grid(row=1, column=2)
+
+clock3 = StillClock(window)
+clock3.grid(row=1, column=3)
+
+clock4 = StillClock(window)
+clock4.grid(row=1, column=4)
+
+window.mainloop()  # Create an event loop
 
