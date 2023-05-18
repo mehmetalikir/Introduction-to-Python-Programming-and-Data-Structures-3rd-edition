8e794d6dc8e676f74852668243fdd883dc5183cd
mehmetalikir <mehmet@senax.com.au>
2023-05-18 10:51:56 +1000

    Update Tkinter: The PieChart class

--- chapter12/Exercise12_19.py
+++ chapter12/Exercise12_19.py
@@ -14,11 +14,8 @@ For data = [[140, "Freshman", "red"], [130, "Sophomore",
 "green"]], the pie chart is as shown in the right part of Figure 12.29. Write a
 test program that displays two pie charts, as shown in Figure 12.29.'''
 
+import math
 from tkinter import *  # Import tkinter
-
-width = 600
-height = 400
-radius = 150
 
 
 class PieChart(Canvas):
@@ -31,15 +28,36 @@ class PieChart(Canvas):
         self.drawPieChart()
 
     def drawPieChart(self):
-        self.create_oval(width / 2 - radius, height / 2 - radius,
-                         width / 2 + radius, height / 2 + radius)
+        values = []
+        for x, y, z in self.__data:
+            values.append(x)
+
+        total = sum(values)
+        start = 0
+        for i in range(len(self.__data)):
+            extent = 360 * self.__data[i][0] / total
+            self.drawAPie(start, extent, self.__data[i][1], self.__data[i][2])
+            start += extent
+
+    def drawAPie(self, start, extent, title, color):
+        width = int(self["width"])
+        height = int(self["height"])
+        radius = min(width, height) * 0.45
+        self.create_arc(width / 2 - radius, height / 2 - radius,
+                        width / 2 + radius, height / 2 + radius,
+                        start=start, extent=extent, fill=color, tags="bar")
+        x = width / 2 + radius * math.cos(math.radians(extent / 2 + start))
+        y = height / 2 - radius * math.sin(math.radians(extent / 2 + start))
+        self.create_text(x, y, text=title, tags="bar")
 
 
 class MainGUI:
     def __init__(self):
         window = Tk()  # Create a window
-        window.title("PieChart Reusable Class")  # Set title
+        window.title("Pie Chart")  # Set a title
 
+        width = 240
+        height = 200
         canvas1 = PieChart(window, [[40, "CS", "red"], [30, "IS", "blue"], [50, "IT", "yellow"]], width=width,
                            height=height)
         canvas1.grid(row=1, column=1)
--- chapter12/Exercise12_20.py
+++ chapter12/Exercise12_20.py
@@ -2,5 +2,11 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Tkinter: Display an n-sided regular polygon) Define a subclass of Canvas,
+named RegularPolygonCanvas, to paint an n-sided regular polygon. The class
+contains a property named numberOfSides, which specifies the number of sides
+in the polygon. The polygon is centered in the canvas, and the polygon’s size is
+proportional to the size of the canvas. Create a triangle, square, pentagon, hexagon,
+heptagon, and octagon from RegularPolygonPanel and display them, as
+shown in Figure 12.30.'''
 
