ddaa3666cb3a8d5a5b5d75de1108207ae934f8cb
mehmetalikir <mehmet@senax.com.au>
2023-05-19 17:32:14 +1000

    :star: Decrypt files :heart_eyes:

--- chapter13/Exercise13_09.py
+++ chapter13/Exercise13_09.py
@@ -2,5 +2,33 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
+'''(Decrypt files) Suppose a file is encrypted using the scheme in Programming
+Exercise 13.8. Write a program to decode an encrypted file. Your program should
+prompt the user to enter an input filename and an output filename and should
+save the unencrypted version of the input file to the output file.'''
 
-''''''
+
+def main():
+    f1 = input("Please enter a source filename: ").strip()
+    f2 = input("Please enter a target filename: ").strip()
+
+    # Open file for input
+    inputFile = open(f1, "r")
+
+    s = inputFile.read()  # Read all from the file
+
+    newS = ""
+
+    for i in range(len(s)):
+        newS += chr(ord(s[i]) - 2)
+
+    inputFile.close()  # Close the input file
+    outputFile = open(f2, "w")
+
+    print(newS, file=outputFile, end="")  # Write to the file
+    print("Done")
+
+    outputFile.close()  # Close the output file
+
+
+main()
