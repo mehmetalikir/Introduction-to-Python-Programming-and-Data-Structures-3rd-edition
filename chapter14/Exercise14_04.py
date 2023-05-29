c30426bc26f0dccae0d0c21bfdd6dc7b569b9281
mehmetalikir <mehmet@senax.com.au>
2023-05-30 07:28:20 +1000

    Listing 14.5

--- chapter14/Exercise14_04.py
+++ chapter14/Exercise14_04.py
@@ -2,4 +2,50 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Tkinter: Count the occurrences of each letter) Rewrite Listing 14.5 using a GUI
+program to let the user enter the file from an entry field, as shown in Figure 14.3a.
+You can also select a file by clicking the Browse button to display an Open file dialog
+box, as shown in Figure 14.3b. The file selected is then displayed in the entry
+field. Clicking the Show Result button displays the result in a text widget. You
+need to display a message in a message box if the file does not exist.'''
+
+
+def main():
+    # Prompt the user to enter a file
+    filename = input("Please enter a filename to read: ").strip()
+    inputFile = open(filename, "r")  # Open the file
+
+    wordCounts = {}  # Create an empty dictionary to count words
+    for line in inputFile:
+        processLine(line.lower(), wordCounts)
+    inputFile.close()
+
+    pairs = list(wordCounts.items())  # Get pairs from the dictionary
+
+    items = [[count, word] for (word, count) in pairs]
+    items.sort(reverse=True)  # Sort pairs in items
+
+    for count, word in items[: 10]:  # Slice the first 10 items
+        print(word, count, sep='\t')
+
+# Count each word in the line
+def processLine(line, wordCounts):
+    line = replacePunctuation(line)  # Replace punctuation with space
+    words = line.split()  # Get words from each line
+    for word in words:
+        if word in wordCounts:
+            wordCounts[word] += 1  # Increase count for word
+        else:
+            wordCounts[word] = 1  # Add an item in the dictionary
+
+
+# Replace punctuation in the line with space
+def replacePunctuation(line):
+    for ch in line:
+        if ch in "~@#$%^&*()_-+=~<>?/,.;:!{}[]|'\"":
+            line = line.replace(ch, " ")
+
+    return line
+
+
+main()  # Call the main function
