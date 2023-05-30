383dec0429390c889517c3add38da3e27cdf4541
mehmetalikir <mehmet@senax.com.au>
2023-05-30 10:06:21 +1000

    Display nonduplicate words in ascending order

--- chapter14/Exercise14_08.py
+++ chapter14/Exercise14_08.py
@@ -2,4 +2,27 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Display nonduplicate words in ascending order) Write a program that prompts
+the user to enter a text file, reads words from the file, and displays all the nonduplicate
+words in ascending order.'''
+
+def main():
+    # Prompt the user to enter filenames
+    f1 = input("Please enter a filename: ").strip()
+
+    # Open files for input
+    inputFile = open(f1, "r")
+
+    s = inputFile.read()  # Read all from the file
+    inputFile.close()
+
+    words = s.split()
+    nonduplicateWords = set(words)
+    words = list(nonduplicateWords)
+    words.sort(reverse=True)
+
+    for word in words:
+        print(word, end=" ")
+
+
+main()
