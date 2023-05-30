4036f16dbddfd087b05a991c4cca940eb175c3b3
mehmetalikir <mehmet@senax.com.au>
2023-05-30 10:33:24 +1000

    Get smallest, largest, and total of a set of numbers

--- chapter14/Exercise14_11.py
+++ chapter14/Exercise14_11.py
@@ -2,4 +2,18 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Get smallest, largest, and total of a set of numbers) Write a program that
+prompts a user to enter 10 whole numbers and store them in a set. Once the numbers
+are entered, the program should display the smallest number entered, the
+largest number entered, and the sum all numbers entered.'''
+
+# Create an empty set
+s1 = set()
+
+for i in range(10):
+    num = int(input("Please enter a whole number: "))
+    s1.add(num)
+
+print(f"The smallest number you entered was: {min(s1)}")
+print(f"The largest number you entered was: {max(s1)}")
+print(f"The sum of all numbers you entered was: {sum(s1)}")
