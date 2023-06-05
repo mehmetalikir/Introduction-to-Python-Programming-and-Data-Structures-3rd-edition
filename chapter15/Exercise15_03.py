f7440ff5bf00a1f16c15fdb61af6a83661833305
mehmetalikir <mehmet@senax.com.au>
2023-06-05 17:31:01 +1000

    Countdown using recursion

--- chapter15/Exercise15_03.py
+++ chapter15/Exercise15_03.py
@@ -2,4 +2,28 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Countdown using recursion) Write a recursive function that counts down from
+a given number to 0. The program should prompt the user for a number and
+print a countdown in *'s from that number to 0 and then print "Blast off!". For
+example, with an input of 4, the program should print:
+****
+***
+**
+*
+Blast off!'''
+
+
+# Count down from a given number to 0
+def countdown(n):
+    if n <= 0:
+        print("Blast off!")  # Base case
+    else:
+        print("*" * n)
+        countdown(n - 1)
+
+
+# Prompt the user for a number
+num = int(input("Please enter a number: "))
+
+# print a countdown in *'s from that number to 0 then print "Blast off!"
+countdown(num)
