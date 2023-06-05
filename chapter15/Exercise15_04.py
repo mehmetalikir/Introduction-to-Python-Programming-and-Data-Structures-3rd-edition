a8b893097614e71f3fe5d070a7ea3fda431dbd7e
mehmetalikir <mehmet@senax.com.au>
2023-06-05 17:47:03 +1000

    Calculate a number to a power

--- chapter15/Exercise15_04.py
+++ chapter15/Exercise15_04.py
@@ -2,4 +2,24 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Calculate a number to a power) Write a function that calculates the result of
+a number multiplied by itself a specified number of times. The program should
+prompt the user for enter a number and the power, calculate the result using the
+recursive function, and display the result.'''
+
+
+# Calculate the result of a number multiplied by itself a specified number of times
+def main():
+    n = int(input("Please enter a number: "))
+    p = int(input("Please enter the power: "))
+    print(f"{n} to the power of {p} is {power(n, p)}")
+
+
+def power(n, p):
+    # Ensures that the recursive method is called until to p reaches 0
+    if p == 0:
+        return 1
+    return n * power(n, p - 1)
+
+
+main()
