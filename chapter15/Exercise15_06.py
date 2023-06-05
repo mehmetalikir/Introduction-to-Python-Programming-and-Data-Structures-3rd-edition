749057dd1e216fed121bf2d2266dadd78b0193fb
mehmetalikir <mehmet@senax.com.au>
2023-06-05 18:13:11 +1000

    Summing series

--- chapter15/Exercise15_06.py
+++ chapter15/Exercise15_06.py
@@ -2,4 +2,23 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Summing series) Write a recursive function to compute the following series:
+
+    m(i) = 1 / 3 + 2 / 4 + ... + i / i + 2
+
+Write a test program that prompts the user to enter an integer for i and displays m(i).'''
+
+
+# Compute the series
+def main():
+    i = int(input("Please enter i: "))
+    print("m(" + str(i) + ") is " + str(m(i)))
+
+
+def m(i):
+    if i == 1:  # Base case
+        return 1.0 / 3
+    return m(i - 1) + i * 1.0 / (i + 2)  # Recursive call
+
+
+main()
