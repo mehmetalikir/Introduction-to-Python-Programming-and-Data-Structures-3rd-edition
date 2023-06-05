3fe72c55447b36808e0cff2c9b31927eeb10f2fd
mehmetalikir <mehmet@senax.com.au>
2023-06-05 19:25:30 +1000

    Find the longest word in a list

--- chapter15/Exercise15_11.py
+++ chapter15/Exercise15_11.py
@@ -2,4 +2,34 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Find the longest word in a list) Write a recursive function that returns the
+longest string in a list. Write a main program that prompts the user to enter a string
+of words and displays the longest word.'''
+
+
+def findLongestWord(words):
+    if len(words) == 1:  # Base case
+        return words[0]
+    else:
+        firstWord = words[0]
+        restWords = words[1:]
+        longestRest = findLongestWord(restWords)  # Recursive call
+
+        if len(firstWord) > len(longestRest):
+            return firstWord
+        else:
+            return longestRest
+
+
+def main():
+    # Prompt the user to enter a string
+    s = input("Please enter a string: ")
+    wordList = s.split()
+
+    longestWord = findLongestWord(wordList)
+
+    # Display result
+    print(f"The longest word in {wordList} is {longestWord}")
+
+
+main()
