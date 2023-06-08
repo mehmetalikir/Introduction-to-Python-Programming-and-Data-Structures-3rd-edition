52e171a22594906d42507d7530e402e03e19cd75
mehmetalikir <mehmet@senax.com.au>
2023-06-08 15:01:13 +1000

    :mortar_board: TODO :x:

---
+++ chapter16/Exercise16_01.py
@@ -0,0 +1,8 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+'''(Maximum consecutive increasingly ordered substring) Write a program that
+prompts the user to enter a string and displays the maximum consecutive
+increasingly ordered substring. Analyze the time complexity of your program.'''
+
