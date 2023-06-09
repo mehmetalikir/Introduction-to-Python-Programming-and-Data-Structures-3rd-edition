c5873646bf6cc26c1f94a9e0b61bf89c7962ab0b
mehmetalikir <mehmet@senax.com.au>
2023-06-09 16:22:45 +1000

    Check for prime numbers

--- chapter16/Exercise16_07.py
+++ chapter16/Exercise16_07.py
@@ -2,4 +2,31 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Check for prime numbers) In programing Exercise 15.7 you were asked to
+implement a recursive function to determine whether a given number is a prime
+number. Now rewrite this implementation to use an algorithm based on iteration
+instead. The program should prompt the user for a number and print whether it is
+prime or not.'''
+
+def main():
+    n = int(input("Please enter a whole number:"))
+
+    if is_prime(n):
+        print(f"{n} is a prime number")
+    else:
+        print(f"{n} is not a prime number")
+
+
+# Check whether a given whole number is a prime number
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for divisor in range(2, int(n ** 0.5) + 1):
+        if n % divisor == 0:
+            return False
+
+    return True
+
+
+main()
