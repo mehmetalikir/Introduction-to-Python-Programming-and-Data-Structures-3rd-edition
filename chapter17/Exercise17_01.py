3412782637c23ccc998921f67706ae57b61136f7
mehmetalikir <mehmet@senax.com.au>
2023-06-11 19:42:21 +1000

    :mortar_board: TODO :x:

---
+++ chapter17/Exercise17_01.py
@@ -0,0 +1,20 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+'''(Nested bubble sort) Write a program which reads test results from a text file and
+sorts them by teh person who took the test then the test results themselves.
+The sorting should be implemented as a bubble sort.
+    The text file contains records in the following format(the name followed by
+four test scores):
+
+    Markus, 45, 47, 65, 77
+    Ariana, 99, 53, 87, 43
+    Eric, 19, 91, 65, 67
+    Irena, 95, 91, 87, 67
+
+    Load the results into a multidimensional list, such as:
+    [['Markus', ['45', '47', '65', '77']]
+
+After performing the sorts, the results should be printed.'''
+
