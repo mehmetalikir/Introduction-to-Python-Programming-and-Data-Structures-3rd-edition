a5cb9cfb5453f8fb0820f1f76ffaac794ddf99dd
mehmetalikir <mehmet@senax.com.au>
2023-06-12 19:14:39 +1000

    :mortar_board: TODO :x:

---
+++ chapter18/Exercise18_01.py
@@ -0,0 +1,18 @@
+# Author: Hidir Sezgin, Mehmet A. Kir
+# Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
+# GitHub: /hidirsezgin, /mehmetalikir
+
+'''(Implement set operations in LinkedList) Define a new class named
+MyLinkedList that extends LinkedList with the following set methods:
+# Add the elements in otherList to this list.
+# Return true if this list changed as a result of the call
+def addAll(self, otherList)
+# Remove all the elements in otherList from this list
+# Return true if this list changed as a result of the call
+def removeAll(self, otherList)
+# Retain the elements in this list that are also in otherList
+# Return true if this list changed as a result of the call
+def retainAll(self, otherList)
+
+Use https://liangpy.pearsoncmg.com/test/Exercise18_01py3e.txt to test your code'''
+
