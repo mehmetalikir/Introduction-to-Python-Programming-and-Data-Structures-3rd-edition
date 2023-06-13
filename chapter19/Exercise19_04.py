a2ddb3a729f906193517f9977b796c9bac752bc2
mehmetalikir <mehmet@senax.com.au>
2023-06-13 13:16:29 +1000

    Implementing inorder using iteration

--- chapter19/Exercise19_04.py
+++ chapter19/Exercise19_04.py
@@ -2,4 +2,86 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Implementing inorder using iteration) Implement the inorder method in BST
+using a stack and iteration instead of recursion.'''
+
+from collections import deque
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    # Insert element e into the binary search tree
+    # Return True if the element is inserted successfully
+    def insert(self, e):
+        if self.root == None:
+            self.root = self.createNewNode(e)  # Create a new root
+        else:
+            # Locate the parent node
+            parent = None
+            current = self.root
+            while current != None:
+                if e < current.element:
+                    parent = current
+                    current = current.left
+                elif e > current.element:
+                    parent = current
+                    current = current.right
+                else:
+                    return False  # Duplicate node not inserted
+
+            # Create the new node and attach it to the parent node
+            if e < parent.element:
+                parent.left = self.createNewNode(e)
+            else:
+                parent.right = self.createNewNode(e)
+
+        self.size += 1  # Increase tree size
+        return True  # Element inserted
+
+    # Create a new TreeNode for element e
+    def createNewNode(self, e):
+        return TreeNode(e)
+
+    # Return the size of the tree
+    def getSize(self):
+        return self.size
+
+    def inorderIterative(self):
+        # create an empty stack
+        stack = deque()
+        current = self.root
+
+        while stack or current:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                current = stack.pop()
+                print(current.element, end=' ')
+                current = current.right
+
+
+class TreeNode:
+    def __init__(self, e):
+        self.element = e
+        self.left = None  # Point to the left node, default None
+        self.right = None  # Point to the right node, default None
+
+
+def main():
+    tree = BST()
+    inputFile = open("JackAndJill.txt", "r")
+
+    fileContent = inputFile.read()
+    inputFile.close()
+    words = fileContent.split()
+    for word in words:
+        tree.insert(word.lower())
+
+    tree.inorderIterative()
+
+
+main()
