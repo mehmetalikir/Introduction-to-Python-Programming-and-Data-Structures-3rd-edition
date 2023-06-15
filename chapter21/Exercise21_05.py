1106da469ee4337330b969233616b02f226558a9
mehmetalikir <mehmet@senax.com.au>
2023-06-15 14:58:14 +1000

    Implement Set

--- chapter21/Exercise21_05.py
+++ chapter21/Exercise21_05.py
@@ -2,4 +2,19 @@
 # Email : hidirsezgin@gmail.com, m.kir@student.unsw.edu.au
 # GitHub: /hidirsezgin, /mehmetalikir
 
-''''''
+'''(Implement Set) Implement the union, difference, and internet methods
+in the Set class'''
+from Set import Set
+
+set1 = Set()
+set1.add(1)
+set1.add(2)
+set1.add(3)
+
+set2 = Set()
+set2.add(2)
+set2.add(3)
+set2.add(4)
+
+print("Set 1:", set1.keys())
+print("Set 2:", set2.keys())
